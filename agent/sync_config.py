d39a7d76ebc4b2a43cd00df52a4476375fb654cb
Yancy Ribbens <yribbens@nagios.com>
2012-12-29 15:41:52 +0000

    create get_plugin func

--- agent/sync_config.py
+++ agent/sync_config.py
@@ -14,14 +14,16 @@ os     = "Windows"
 os_ver = "6.1.7601"
 arch   = "x86"
 
-data = dict(token=token, cmd=cmd, plugin=plugin, os=os, os_ver=os_ver, arch=arch)
-#resp, content = h.request(url, "POST", urlencode(data))
 
-retrieve_url   = url + urlencode( data )
-full_plug_path = "./plugins/" + plugin
+def get_plugin():
+	data = dict(token=token, cmd=cmd, plugin=plugin, os=os, os_ver=os_ver, arch=arch)
+	
+	retrieve_url   = url + urlencode( data )
+	full_plug_path = "./plugins/" + plugin
+	
+	urllib.urlretrieve (retrieve_url, full_plug_path)
 
-urllib.urlretrieve (retrieve_url, full_plug_path)
-
+get_plugin()
 
 #CHUNK = 16 * 1024
 #with open(plugin, 'wb') as fp:
@@ -30,4 +32,4 @@ urllib.urlretrieve (retrieve_url, full_plug_path)
 #    if not chunk: break
 #    fp.write(CHUNK)
 
-print "http://192.168.2.29/nrdp/?token=e9c6oudjfjs6&cmd=getplugin&plugin=check_winping.exe&os=Windows&os_ver=6.1.7601&arch=x86"
+#print "http://192.168.2.29/nrdp/?token=e9c6oudjfjs6&cmd=getplugin&plugin=check_winping.exe&os=Windows&os_ver=6.1.7601&arch=x86"
