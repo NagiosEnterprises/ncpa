3c4f8d2338bcf1e69475346ce48632e83c4357da
Yancy Ribbens <yribbens@nagios.com>
2013-01-01 03:27:22 +0000

    streamline args

--- agent/nrds_conduit.py
+++ agent/nrds_conduit.py
@@ -5,39 +5,34 @@ import urllib
 import requests
 
 class nrds():
-    def __init__(self, token, nrdp_url):
-        self.token      = token
-        self.nrdp_url   = nrdp_url
-        self.plugin_loc = "./plugins/"
-        self.os		  = "Chinook"
-
+    def __init__(self, token, nrdp_url, plugin_loc="./plugins/", os="Chinook"):
+        
+        self.nrds_settings = { 'token':token, 'nrdp_url':nrdp_url, 'plugin_loc':plugin_loc, 'os':os } 
+    
     def get_plug(self, *args, **kwargs):
-        #print args["plugin"]
+        kwargs["cmd"]   = "getplugin"
         
-        kwargs["token"] = self.token
-        kwargs["cmd"] = "getplugin"
-        kwargs["os"]  = self.os
-        print kwargs
-        
-        self.url_request         = requests.post(self.nrdp_url, params=kwargs)
-        self.local_path_location = self.plugin_loc + kwargs['plugin']
-        
-        print self.url_request
+        self.url_request = requests.post(
+            self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
+            )
+            
+        self.local_path_location = self.nrds_settings['plugin_loc'] + kwargs['plugin']
         
         with open(self.local_path_location, 'w') as plugin:
             plugin.write(self.url_request.content)
             
-    def query_available_plugins():
-        #this will fetch the config and return an array of
-        #available plugins
-        
-    def update_available():
-        #predicate procedure which will return T or F if
-        #config is available
-        
-    def fetch_config():
-        #will fetch updated config
-        
+    #~ def query_available_plugins(self, *args, **kwargs):
+        #~ #http://192.168.2.29/nrdp//?token=k2suan32qt50&cmd=updatenrds&XMLDATA=%3C?xml%20version='1.0'%20?%3E%3Cconfigs%3E%3Cconfig%3E%3Cname%3Ewindows%3C/name%3E%3Cversion%3E0.1%3C/version%3E%3C/config%3E%3C/configs%3E
+        #~ #this will fetch the config and return an array of
+        #~ #available plugins
+        #~ 
+    #~ def update_available():
+        #~ #predicate procedure which will return T or F if
+        #~ #config is available
+        #~ 
+    #~ def fetch_config():
+        #~ #will fetch updated config
+        #~ 
     
         
     
