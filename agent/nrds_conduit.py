5d6f2c03f3f53f85df1e5357eb66e48d0101fe1d
Yancy Ribbens <yribbens@nagios.com>
2013-01-01 06:34:26 +0000

    pull config

--- agent/nrds_conduit.py
+++ agent/nrds_conduit.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-from urllib import urlencode
-import urllib
 import requests
+from xml.dom.minidom import Document
 
 class nrds():
-    def __init__(self, token, nrdp_url, plugin_loc="./plugins/", os="Chinook"):
-        
-        self.nrds_settings = { 'token':token, 'nrdp_url':nrdp_url, 'plugin_loc':plugin_loc, 'os':os } 
+    def __init__(self, token, nrdp_url):
+        self.nrds_settings = { 'token':token, 'nrdp_url':nrdp_url } 
     
     def get_plug(self, *args, **kwargs):
-        kwargs["cmd"]   = "getplugin"
+        kwargs["cmd"] = "getplugin"
+        kwargs["os"]  = "chinook"
         
         self.url_request = requests.post(
             self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
@@ -20,8 +19,35 @@ class nrds():
         
         with open(self.local_path_location, 'w') as plugin:
             plugin.write(self.url_request.content)
-            
-    #~ def query_available_plugins(self, *args, **kwargs):
+
+
+    def fetch_config(self, *args, **kwargs):
+        kwargs['cmd'] = 'updatenrds'
+        
+        doc = Document()
+        configs = doc.createElement("configs")
+        doc.appendChild(configs)
+        config = doc.createElement("config")
+        configs.appendChild(config)
+        name = doc.createElement("name")
+        config.appendChild(name)
+        name_text = doc.createTextNode(kwargs['config_name'])
+        name.appendChild(name_text)
+        version = doc.createElement("version")
+        config.appendChild(version)
+        version_number = doc.createTextNode(kwargs['config_version'])
+        version.appendChild(version_number)
+
+        kwargs['XMLDATA'] = doc.toprettyxml(indent="")
+        
+        self.url_request = requests.post(
+            self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
+            )
+        
+        with open(kwargs['path'], 'w') as config:
+            config.write(self.url_request.content)
+
+        #top = http://192.168.2.29/nrdp//?token=k2suan32qt50&cmd=updatenrds&XMLDATA=<?xml version='1.0' ?><configs><config><name>windows</name><version>0.2</version></config></configs>
         #~ #http://192.168.2.29/nrdp//?token=k2suan32qt50&cmd=updatenrds&XMLDATA=%3C?xml%20version='1.0'%20?%3E%3Cconfigs%3E%3Cconfig%3E%3Cname%3Ewindows%3C/name%3E%3Cversion%3E0.1%3C/version%3E%3C/config%3E%3C/configs%3E
         #~ #this will fetch the config and return an array of
         #~ #available plugins
