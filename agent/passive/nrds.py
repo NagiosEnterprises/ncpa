9d2cbed8e67ddee374aea3d65b82cea7b3bab002
Yancy Ribbens <yribbens@nagios.com>
2013-01-03 08:40:09 +0000

    use utils.py and config parser to get args from file

--- agent/passive/nrds.py
+++ agent/passive/nrds.py
@@ -5,9 +5,10 @@ import abstract
 import xmltodict
 from xml.dom.minidom import Document
 from xml.dom.minidom import parseString
+import utils
 
 plugin_loc ='./plugins'
-path       ='./etc/ncpa_test.cfg'
+path       ='../etc/ncpa_test.cfg'
 
 class Handler( abstract.NagiosHandler ):
     """
@@ -16,7 +17,10 @@ class Handler( abstract.NagiosHandler ):
     
     def __init__(self, *args, **kwargs):
         super(Handler, self).__init__(*args, **kwargs)
-        #~ self.nrds_settings = { 'token':token, 'nrdp_url':nrdp_url }
+        
+        token = self.config.get('nrdp', 'nrdp_token')
+        nrdp_url = self.config.get('nrdp', 'nrdp_server')
+        self.nrds_settings = { 'token':token, 'nrdp_url':nrdp_url }
         
     def run(self, *args, **kwargs):
         self.get_plug()
@@ -39,7 +43,9 @@ class Handler( abstract.NagiosHandler ):
 
     def fetch_config(self, *args, **kwargs):
         """
-        fetch config takes config name as argument and downloads new config to
+        fetch config takes config name as argument
+        for example configname=ncpa
+        and downloads new config to
         whatever is declared as path
         """
         
@@ -47,11 +53,15 @@ class Handler( abstract.NagiosHandler ):
         
         kwargs['cmd'] = 'getconfig'
         kwargs['os']  = 'chinook'
+        kwargs["token"] = self.nrds_settings["token"]
         
-        self.url_request = requests.post(
-            self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
-            )
-            
+        #post_this = dict( self.nrds_settings.items() + kwargs.items() )
+        
+        print kwargs
+        print self.nrds_settings['nrdp_url']
+         
+        self.url_request = utils.send_nrdp( self.nrds_settings['nrdp_url'], **kwargs )
+
         with open(path, 'w') as config:
             config.write(self.url_request.content)
             
@@ -60,28 +70,29 @@ class Handler( abstract.NagiosHandler ):
         takes current config version as argument and returns T or F if new config is available
         """
         
+        print self.nrds_settings
         kwargs['cmd'] = 'updatenrds'
         kwargs['os']  = 'chinook'
         kwargs['config_name'] = 'ncpa'
         
         kwargs['XMLDATA']  = self.build_xml( kwargs )
         
-        self.url_request = requests.post(
-            self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
-            )
-            
+        #~ self.url_request = requests.post(
+            #~ self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
+            #~ )
+            #~ 
         #~ print self.url_request.content
-         
-        self.config_dict = xmltodict.parse( self.url_request.content )
-        self.status      = self.config_dict['result']['status']
-        
-        if self.status == "1":
-            return True
-        else:
-            return False
+         #~ 
+        #~ self.config_dict = xmltodict.parse( self.url_request.content )
+        #~ self.status      = self.config_dict['result']['status']
         #~ 
-        with open('/tmp/config.xml', 'w') as config:
-            config.write(self.url_request.content)
+        #~ if self.status == "1":
+            #~ return True
+        #~ else:
+            #~ return False
+            #~ 
+        #~ with open('/tmp/config.xml', 'w') as config:
+            #~ config.write(self.url_request.content)
         #~ 
         #~ #http://192.168.2.29/nrdp//?token=k2suan32qt50&cmd=updatenrds&XMLDATA=<?xml version='1.0' ?><configs><config><name>windows</name><version>0.2</version></config></configs>
         #~ 
@@ -96,9 +107,6 @@ class Handler( abstract.NagiosHandler ):
         self.url_request = requests.post(
             self.nrds_settings['nrdp_url'], params=dict( self.nrds_settings.items() + kwargs.items() )
             )
-            
-    def print_local_config(self):
-        print self.config
         
     def build_xml(self, settings_dict):
         
