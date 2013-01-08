e95c786825c558b407a2c69ef2289885917f0111
Nicholas Scott <scot0357@gmail.com>
2013-01-08 07:25:35 +0000

    Did some housekeeping on NRDS and updated it to reflect the updated config directives. nrdp_token -> token, nrdp_server -> parent.

--- agent/passive/nrds.py
+++ agent/passive/nrds.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-
 import requests
 import abstract
 import xmltodict
 import utils
 
-class Handler( abstract.NagiosHandler ):
+class Handler(abstract.NagiosHandler):
     """
     api for nrds config management
     """
@@ -13,8 +12,8 @@ class Handler( abstract.NagiosHandler ):
     def __init__(self, *args, **kwargs):
         super(Handler, self).__init__(*args, **kwargs)
         
-        self.token = self.config.get('nrdp', 'nrdp_token')
-        self.nrdp_url = self.config.get('nrdp', 'nrdp_server')
+        self.token = self.config.get('nrdp', 'token')
+        self.nrdp_url = self.config.get('nrdp', 'parent')
         
     def run(self, *args, **kwargs):
         self.get_plug()
@@ -27,17 +26,17 @@ class Handler( abstract.NagiosHandler ):
         kwargs['os']  = "Chinook"
         kwargs['token'] = self.token
         
-        self.url_request = utils.send_nrdp( self.nrdp_url, **kwargs )
+        self.url_request = utils.send_nrdp(self.nrdp_url, **kwargs)
         self.local_path_location = self.plugin_loc + kwargs['plugin']
         
         with open(self.local_path_location, 'w') as plugin:
             plugin.write(self.url_request.content)
 
     def getconfig(self, *args, **kwargs):
-        """
-        Downloads new config to
-        whatever is declared as path
-        """
+        '''Downloads new config to whatever is declared as path
+        
+        @todo Validate config before saving
+        '''
         
         kwargs['configname'] = self.config.get('nrds', 'config_name')
         
@@ -48,35 +47,31 @@ class Handler( abstract.NagiosHandler ):
         kwargs["token"] = self.token
         
         self.url_request = utils.send_nrdp( self.nrdp_url, **kwargs )
-        print self.url_request.url
+        self.logger.debug('URL I am requesting: %s' % self.url_request.url)
+        self.logger.debug('Content returned: %s' % self.url_request.content)
         
-        print self.url_request.content
-        
-        #TODO validate config before saving
         if self.url_request.content != "":
             with open( self.config.file_path , 'w') as config:
                 config.write(self.url_request.content)
                 
     def updatenrds(self, *args, **kwargs):
-        """
-        takes current config version as argument and returns T or F if new config is available
-        """
+        '''Takes current config version as argument and returns T or F 
+        if new config is available
         
+        @todo Log results if we do not have this config
+        '''
         kwargs['token'] = self.token
-        kwargs['cmd'] = self.updatenrds.__name__
+        kwargs['cmd'] = 'updatenrds'
         kwargs['os']  = 'chinook'
-        kwargs['configname'] = self.config.get( 'nrds' , 'config_name' )
-        kwargs['version'] = self.config.get( 'nrds', 'config_version' )
+        kwargs['configname'] = self.config.get('nrds', 'config_name')
+        kwargs['version'] = self.config.get('nrds', 'config_version')
 
-        self.url_request = utils.send_nrdp( self.nrdp_url, **kwargs )
+        self.url_request = utils.send_nrdp(self.nrdp_url, **kwargs)
         
-        #TODO log results for we do not have this config
-        #print self.url_request.content
-         
-        self.config_dict = xmltodict.parse( self.url_request.content )
+        self.config_dict = xmltodict.parse(self.url_request.content)
         self.status      = self.config_dict['result']['status']
         
-        print self.status
+        self.logger.debug('Status number of new config check: %s' % str(self.status))
         
         if self.status == "1":
             return True
