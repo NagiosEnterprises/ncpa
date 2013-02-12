7c7c6f32f9a0fff98f72fa7498e00c3248c46f35
Nicholas Scott <scot0357@gmail.com>
2013-02-12 20:24:59 +0000

    Made the server process spawn a separate thread

--- agent/ncpa_listener_windows.py
+++ agent/ncpa_listener_windows.py
@@ -94,7 +94,8 @@ class aservice(win32serviceutil.ServiceFramework):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,servicemanager.PYS_SERVICE_STARTED,(self._svc_name_, '')) 
       
         self.timeout = 3000
-        self.start()
+        server_thread = threading.Thread(target=self.start)
+        server_thread.start()
         while 1:
             # Wait for service stop signal, if I timeout, loop again
             rc = win32event.WaitForSingleObject(self.hWaitStop, self.timeout)
