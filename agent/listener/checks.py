52bbb4ada4cff3865c0b3800071dd303050a7185
Nicholas Scott <scot0357@gmail.com>
2013-03-08 17:12:38 +0000

    Removed unused check_x

--- agent/listener/checks.py
+++ agent/listener/checks.py
@@ -1,28 +1,6 @@
-import psutil
 import subprocess
 import logging
 import ConfigParser
-
-def check_cpu(item, *args, **kwargs):
-    item.values = psutil.cpu_percent(percpu=True)
-    item.unit = '%'
-    item.nice = 'CPU_Load'
-    item.set_stdout('CPU Utilization is at')
-    return item
-
-def check_swap(item, *args, **kwargs):
-    item.unit = '%'
-    item.nice = 'Swap_Usage'
-    item.set_values(psutil.swap_memory().percent)
-    item.set_stdout('Swap Usage is at')
-    return item
-
-def check_memory(item, *args, **kwargs):
-    item.unit = '%'
-    item.nice = 'Memory_Usage'
-    item.set_values(psutil.virtual_memory().percent)
-    item.set_stdout('Physical Memory Usage is at')
-    return item
     
 def check_custom(item, plugin_name, plugin_args, config, *args, **kwargs):
     """
