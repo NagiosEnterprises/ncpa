567a8aeaf0e2a630e337b7af39f1c3c005edf394
Nicholas Scott <scot0357@gmail.com>
2013-05-29 20:57:01 +0000

    Adding deb packaging requirements.

--- agent/build_resources/build_package.sh
+++ agent/build_resources/build_package.sh
@@ -3,5 +3,24 @@ then
     cp build_resources/postinstall-pak .
     cp build_resources/postremove-pak .
     cp build_resources/description-pak .
-    checkinstall --pkgname=ncpa --strip=no --stripso=no --exclude=/var,/dev,/tmp --nodoc -R cp build/exe.linux-i686-2.6 /usr/local/ncpa -r
+    checkinstall    --pkgname=ncpa \
+                    --strip=no \
+                    --stripso=no \
+                    --exclude=/var,/dev,/tmp \
+                    --nodoc \
+                    -R \
+                    cp build/exe.linux-i686-2.6 /usr/local/ncpa -r
+elif [ "$1" == "pkg" ];
+then
+    echo "Moving deb installs to current dir..."
+    cp build_resources/postinstall-pak-deb postinstall-pak
+    cp build_resources/postremove-pak-deb postremove-pak
+    cp build_resources/description-pak .
+    checkinstall    --pkgname=ncpa \
+                    --strip=no \
+                    --stripso=no \
+                    --exclude=/var,/dev,/tmp \
+                    --nodoc \
+                    -D \
+                    cp build/exe.linux-i686-2.6 /usr/local/ncpa -r
 fi
