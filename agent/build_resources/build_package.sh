98e34af3f8ad73bde55bd8f9ef800a906663c2c2
Nicholas Scott <scot0357@gmail.com>
2013-05-29 21:25:49 +0000

    Shutting this back and forth between d and C

--- agent/build_resources/build_package.sh
+++ agent/build_resources/build_package.sh
@@ -16,11 +16,12 @@ then
     cp build_resources/postinstall-pak-deb postinstall-pak
     cp build_resources/postremove-pak-deb postremove-pak
     cp build_resources/description-pak .
+    mv build/exe.linux-i686-2.6 build/ncpa
     checkinstall    --pkgname=ncpa \
                     --strip=no \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
                     --nodoc \
                     -D \
-                    cp build/exe.linux-i686-2.6/* /usr/local/ncpa/* -r
+                    cp build/ncpa /usr/local/ -r
 fi
