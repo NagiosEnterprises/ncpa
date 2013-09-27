52cd68be0e8fea105089d5845b99d3430118a4a5
Nicholas Scott <scot0357@gmail.com>
2013-09-27 17:52:32 +0000

    Modified the builder to be able to build both deb and rpm at the same time

--- agent/build_resources/build_package.sh
+++ agent/build_resources/build_package.sh
@@ -1,4 +1,4 @@
-if [ "$1" == "rpm" ];
+if [ "$1" == "rpm" ] || [ "$2" == "rpm" ];
 then
     cp build_resources/postinstall-pak .
     cp build_resources/postremove-pak .
@@ -7,21 +7,24 @@ then
                     --strip=no \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
+                    --pakdir=. \
                     --nodoc \
                     -R \
                     cp build/exe.linux-i686-2.6 /usr/local/ncpa -r
-elif [ "$1" == "pkg" ];
+fi
+
+if [ "$1" == "pkg" ] || [ "$2" == "pkg" ];
 then
     echo "Moving deb installs to current dir..."
     cp build_resources/postinstall-pak-deb postinstall-pak
     cp build_resources/postremove-pak-deb postremove-pak
     cp build_resources/description-pak .
-    mv build/exe.linux-i686-2.6 build/ncpa
     checkinstall    --pkgname=ncpa \
                     --strip=no \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
                     --nodoc \
+                    --pakdir=. \
                     -D \
-                    cp build/ncpa /usr/local/ -r
+                    cp build/exe.linux-i686-2.6 /usr/local/ncpa -r
 fi
