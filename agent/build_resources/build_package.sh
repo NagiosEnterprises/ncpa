1c69da869b6be7a23b939961612c35bd543b5686
Nicholas Scott <scot0357@gmail.com>
2013-09-27 23:06:31 +0000

    Fixed some possible license loophole

--- agent/build_resources/build_package.sh
+++ agent/build_resources/build_package.sh
@@ -3,12 +3,15 @@ then
     cp build_resources/postinstall-pak .
     cp build_resources/postremove-pak .
     cp build_resources/description-pak .
+    cp build_resources/NagiosSoftwareLicense.txt build/exe.linux-i686-2.6/
     checkinstall    --pkgname=ncpa \
                     --strip=no \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
                     --pakdir=. \
                     --nodoc \
+                    --maintainer=nscott@nagios.com \
+                    --pkglicense='Nagios Open Source License' 
                     -R \
                     cp build/exe.linux-i686-2.6 /usr/local/ncpa -r
 fi
@@ -19,9 +22,12 @@ then
     cp build_resources/postinstall-pak-deb postinstall-pak
     cp build_resources/postremove-pak-deb postremove-pak
     cp build_resources/description-pak .
+    cp build_resources/NagiosSoftwareLicense.txt build/exe.linux-i686-2.6/
     checkinstall    --pkgname=ncpa \
                     --install=no \
                     --strip=no \
+                    --maintainer=nscott@nagios.com \
+                    --pkglicense='Nagios Open Source License' \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
                     --nodoc \
