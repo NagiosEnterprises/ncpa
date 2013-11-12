c2177d38eb0f370d629f2fbbfdbcc91718f5b5a1
Nicholas Scott <scot0357@gmail.com>
2013-11-12 23:51:03 +0000

    Updating to enfore Python2.6 and some of the help files.

--- agent/build_resources/build_package.sh
+++ agent/build_resources/build_package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=`python2.6 -c 'import listener.server as a;print a.__VERSION__'`
+
 if [ "$1" == "rpm" ] || [ "$2" == "rpm" ];
 then
     cp build_resources/postinstall-pak .
@@ -7,14 +9,18 @@ then
     cp build_resources/description-pak .
     cp build_resources/NagiosSoftwareLicense.txt build/exe.linux-*/
     checkinstall    --pkgname=ncpa \
+                    --install=no \
                     --strip=no \
+                    --arch=`arch` \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
+                    --pkgversion="$VERSION" \
                     --pakdir=. \
                     --nodoc \
                     --maintainer=nscott@nagios.com \
                     --pkglicense='Nagios Open Source License' \
                     -R \
+                    -y \
                     cp build/exe.linux-* /usr/local/ncpa -r
 fi
 
@@ -28,12 +34,15 @@ then
     checkinstall    --pkgname=ncpa \
                     --install=no \
                     --strip=no \
+                    --arch=`arch` \
                     --maintainer=nscott@nagios.com \
+                    --pkgversion="$VERSION" \
                     --pkglicense='Nagios Open Source License' \
                     --stripso=no \
                     --exclude=/var,/dev,/tmp \
                     --nodoc \
                     --pakdir=. \
                     -D \
+                    -y \
                     cp build/exe.linux-* /usr/local/ncpa -r
 fi
