49e1fc52944fb6219634857c2ea96ba39783fee6
Nicholas Scott <scot0357@gmail.com>
2013-11-14 02:08:00 +0000

    Fixing the windows make.bat to refer explicitly to the sphinx-build

--- docs/make.bat
+++ docs/make.bat
@@ -3,7 +3,7 @@
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD=C:\Python27\Scripts\sphinx-build
 )
 set BUILDDIR=_build
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
