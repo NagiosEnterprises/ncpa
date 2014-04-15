c7933a9266aee035d87f13597cbdd5ca7a161cd0
Nicholas Scott <scot0357@gmail.com>
2014-04-15 21:31:19 +0000

    Adding the change to the docs config file.

--- apply_version.py
+++ apply_version.py
@@ -9,11 +9,19 @@ VERSION = version_file.readline().strip()
 version_file.close()
 
 server_source = os.path.join(BASEDIR, 'agent', 'listener', 'server.py')
+docs_config = os.path.join(BASEDIR, 'docs', 'conf.py')
 
-print server_source
 # Now edit the listener/server.py to have that version
 for line in fileinput.input(server_source, inplace=True):
     if line.startswith('__VERSION__'):
-        print '__VERSION__ = %s' % VERSION,
+        print '__VERSION__ = %s' % VERSION
+    else:
+        print line,
+
+for line in fileinput.input(docs_config, inplace=True):
+    if line.startswith('version ='):
+        print 'version = %s' % VERSION
+    elif line.startswith('release ='):
+        print 'release = %s' % VERSION
     else:
         print line,
