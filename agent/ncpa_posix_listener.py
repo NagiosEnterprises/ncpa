3d7f98cb01cd251db1a5c54369d179af086c403b
Nicholas Scott <scot0357@gmail.com>
2014-05-08 17:01:15 -0500

    Moved away from the Flask development server. Now using Tornado, can handle many more request and does not get all crufty when run for more than a week

--- agent/ncpa_posix_listener.py
+++ agent/ncpa_posix_listener.py
@@ -5,6 +5,7 @@ import os
 import ncpadaemon
 import listener.server
 import filename
+import listener.certificate
 import werkzeug.serving
 import ConfigParser
 from tornado.wsgi import WSGIContainer
@@ -15,32 +16,33 @@ from tornado.ioloop import IOLoop
 import jinja2.ext
 
 class Listener(ncpadaemon.Daemon):
-    default_conf = os.path.abspath(os.path.join(filename.get_dirname_file(), u'etc', u'ncpa.cfg'))
+    default_conf = os.path.abspath(os.path.join(filename.get_dirname_file(), 'etc', 'ncpa.cfg'))
     section = u'listener'
     
     def run(self):
-        address = self.config_parser.get(u'listener', u'ip')
-        port = int(self.config_parser.get(u'listener', u'port'))
-        certificate = self.config_parser.get(u'listener', u'certificate')
+        address = self.config_parser.get('listener', 'ip')
+        port = int(self.config_parser.get('listener', 'port'))
+        user_cert = self.config_parser.get('listener', 'certificate')
 
-        if certificate == u'adhoc':
-            ssl_context = u'adhoc'
+        if user_cert == 'adhoc':
+            basepath = filename.get_dirname_file()
+            cert, key = listener.certificate.create_self_signed_cert(basepath, 'ncpa.crt', 'ncpa.key')
         else:
-            ssl_context = certificate.split(u',')
+            cert, key = user_cert.split(',')
+        ssl_context = {'certfile': cert, 'keyfile': key}
 
         try:
-            logging.info(u'Starting server...')
-            listener.server.listener.config_filename = u'etc/ncpa.cfg'
-            listener.server.listener.config[u'iconfig'] = self.config_parser
+            logging.info('Starting server...')
+            listener.server.listener.config_filename = 'etc/ncpa.cfg'
+            listener.server.listener.config['iconfig'] = self.config_parser
             listener.server.listener.secret_key = os.urandom(24)
             try:
-                http_server = HTTPServer(WSGIContainer(listener.server.listener))
+                http_server = HTTPServer(WSGIContainer(listener.server.listener),
+                                         ssl_options=ssl_context)
                 http_server.listen(port)
                 IOLoop.instance().start()
-                #listener.server.listener.run(address, port, ssl_context=ssl_context)
             except Exception, e:
                 logging.exception(e)
-                listener.server.listener.run(address, port)
         except Exception, e:
             logging.exception(e)
 
