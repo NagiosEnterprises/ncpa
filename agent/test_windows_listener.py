2d4721a76247e29d7de3ba5f678074a94741619c
Nicholas <scot0357@gmail.com>
2014-05-22 16:54:28 -0500

    Updated ncpa_windows.py to use gevent, and did various cleanup duties, removing any references to self.logger, using the logging module instead.

--- agent/test_windows_listener.py
+++ agent/test_windows_listener.py
@@ -1,18 +1,5 @@
-from gevent.wsgi import WSGIServer
-import listener.server
-import listener.psapi
-import listener.windowscounters
-import os
+import ncpa_windows
 
-counters = listener.windowscounters.get_counters_node()
-listener.psapi.init_root(counters)
-
-plugin_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                           'plugins')
-
-listener.server.listener.config['iconfig'] = {'api': 'test',
-                                              'plugin directives': plugin_path}
-listener.server.listener.secret_key = 'notasecret'
-
-http_server = WSGIServer(('', 5693), listener.server.listener)
-http_server.serve_forever()
+listener = ncpa_windows.Listener(debug=True)
+listener.Initialize(None)
+listener.start()
