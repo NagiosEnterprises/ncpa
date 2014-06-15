67c4f6477c01e9b3a6400a6928cccc594cf4dbb3
Nicholas Scott <scot0357@gmail.com>
2014-06-15 15:19:11 -0400

    Fixing up the tests. Getting the NRDP passive checks up to snuff.

--- agent/tests/test_passive_nrdp.py
+++ agent/tests/test_passive_nrdp.py
@@ -1,79 +1,18 @@
+import os
+import sys
 import unittest
 
-class TestNRDPAssociation(unittest.TestCase):
-    def test___init__(self):
-        # n_rdp_association = NRDPAssociation(*args, **kwargs)
-        assert False # TODO: implement your test here
+basepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 
-class TestHandler(unittest.TestCase):
-    def test___init__(self):
-        # handler = Handler(*args, **kwargs)
-        assert False # TODO: implement your test here
+sys.path.append(basepath)
+from passive.abstract import *
 
-    def test_log_result(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.log_result(retxml, *args, **kwargs))
-        assert False # TODO: implement your test here
 
-    def test_make_tag(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.make_tag(tagname, text, tagattr, *args, **kwargs))
-        assert False # TODO: implement your test here
+class TestNRDPHandler(unittest.TestCase):
 
-    def test_make_xml(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.make_xml(result, *args, **kwargs))
-        assert False # TODO: implement your test here
+    def setUp(self):
+        self.handler = nrdp.Handler()
 
-    def test_run(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.run(*args, **kwargs))
-        assert False # TODO: implement your test here
 
-    def test_set_xml_of_checkresults(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.set_xml_of_checkresults(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_submit_to_nagios(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.submit_to_nagios(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_config_update_is_required(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.config_update_is_required(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_get_installed_plugins(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.get_installed_plugins(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_get_os(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.get_os())
-        assert False # TODO: implement your test here
-
-    def test_get_plugin(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.get_plugin(plugin, *args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_get_required_plugins(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.get_required_plugins(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_list_missing_plugins(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.list_missing_plugins(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-    def test_update_config(self):
-        # handler = Handler(*args, **kwargs)
-        # self.assertEqual(expected, handler.update_config(*args, **kwargs))
-        assert False # TODO: implement your test here
-
-if __name__ == u'__main__':
+if __name__ == '__main__':
     unittest.main()
