086ffe852e292b205a6e2750e3c3fac0dc428b69
Nicholas Scott <scot0357@gmail.com>
2014-06-28 03:05:15 -0400

    Removing old useless tests.

--- agent/tests/test_listener_psextensions.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestGetServices(unittest.TestCase):
-    def test_get_services(self):
-        # self.assertEqual(expected, get_services())
-        assert False # TODO: implement your test here
-
-if __name__ == u'__main__':
-    unittest.main()
