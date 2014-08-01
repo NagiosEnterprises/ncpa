2fa12425b324b8dec927610b08e8df9055776171
tech team <techteam@nagios.com>
2014-08-01 12:33:19 -0500

    Changes to windows build

--- apply_version.py
+++ apply_version.py
@@ -14,14 +14,14 @@ docs_config = os.path.join(BASEDIR, 'docs', 'conf.py')
 # Now edit the listener/server.py to have that version
 for line in fileinput.input(server_source, inplace=True):
     if line.startswith('__VERSION__'):
-        print '__VERSION__ = %s' % VERSION
+        print "__VERSION__ = '%s'" % VERSION
     else:
         print line,
 
 for line in fileinput.input(docs_config, inplace=True):
     if line.startswith('version ='):
-        print 'version = %s' % VERSION
+        print "version = %s" % VERSION[:3]
     elif line.startswith('release ='):
-        print 'release = %s' % VERSION
+        print "release = '%s'" % VERSION
     else:
         print line,
