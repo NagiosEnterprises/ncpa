0fb0c7ebcaf360b38392cb8f34ad6c92275fcb2d
unknown <dberger@dberger.valvesoftware.com>
2014-12-23 10:39:31 -0800

    tweaks to the build setup script

--- build-setup.bat
+++ build-setup.bat
@@ -1,4 +1,15 @@
-set PATH=%PATH%;c:\Python27;c:\Python27\scripts;c:\OpenSSL-Win64\bin;
+@echo off
+if not exist "%pydir%" (
+  echo Set %%pydir%% to point to the desired python install
+  exit /B 1
+)
+set PATH=%pydir%;%pydir%\scripts;%PATH%
+
+if not exist "%openssldir%" (
+  echo Set %%openssldir%% to point to the desired OpenSSL install
+  exit /B 1
+)
+set PATH=%openssldir%\bin;%PATH%
 
 if not exist "%PROGRAMFILES%\NSIS" (
   echo Install NSIS 2.46 from http://sourceforge.net/projects/nsis/files/NSIS%202/2.46/
@@ -28,3 +39,5 @@ pip install cx_Logging --allow-external cx-Logging --allow-unverified cx-Logging
 pip install cx_PyGenLib --allow-external cx-PyGenLib --allow-unverified cx-PyGenLib
 pip install nose
 
+echo to build ncpa:
+echo python build\build_windows.py
