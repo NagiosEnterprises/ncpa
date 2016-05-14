454d0cd37ae05bd85d193679d77e2ca35ed16ffb
Jake Omann <jomann@nagios.com>
2016-05-14 04:46:10 -0500

    Update to use urllib2/urllib.request
    Instead of using urllib's urlretrieve which seems to be causing a lot of errors while trying to connect multiple times, changed it to use urlopen which is in python 2.x in urllib2 and in python 3.x in urllib.request. Testing indicates a much better chance of successful connection than originally.

--- client/check_ncpa.py
+++ client/check_ncpa.py
@@ -8,27 +8,30 @@ import sys
 import optparse
 import traceback
 
+# Python 2/3 Compatibility imports
+
 try:
     import json
 except ImportError:
     import simplejson as json
 
-# Python2/3 Compatibility imports
 try:
     import urllib.request
     import urllib.parse
     import urllib.error
 except ImportError:
+    import urllib2
     import urllib
 
 try:
     urlencode = urllib.parse.urlencode
 except AttributeError:
     urlencode = urllib.urlencode
+
 try:
-    urlretrieve = urllib.request.urlretrieve
+    urlopen = urllib.request.urlopen
 except AttributeError:
-    urlretrieve = urllib.urlretrieve
+    urlopen = urllib2.urlopen
 
 try:
     urlquote = urllib.parse.quote
@@ -39,9 +42,7 @@ import shlex
 import re
 import signal
 
-
-__VERSION__ = '0.3.6'
-
+__VERSION__ = '1.0.0'
 
 def pretty(d, indent=0, indenter=' ' * 4):
     info_str = ''
@@ -59,71 +60,54 @@ def parse_args():
     version = 'check_ncpa.py, Version %s' % __VERSION__
 
     parser = optparse.OptionParser()
-    parser.add_option(
-        "-H", "--hostname", help="The hostname to be connected to.")
-    parser.add_option(
-        "-M", "--metric", default='',
-        help="The metric to check, this is defined on client "
-        "system. This would also be the plugin name in the "
-        "plugins directory. Do not attach arguments to it, "
-        "use the -a directive for that. DO NOT INCLUDE the api/ "
-        "instruction.")
-    parser.add_option(
-        "-P", "--port", default=5693, type="int",
-        help="Port to use to connect to the client.")
-    parser.add_option(
-        "-w", "--warning", default=None, type="str",
-        help="Warning value to be passed for the check.")
-    parser.add_option(
-        "-c", "--critical", default=None, type="str",
-        help="Critical value to be passed for the check.")
-    parser.add_option(
-        "-u", "--units", default=None,
-        help="The prefix (M, G, T) for the bytes/bits units.")
-    parser.add_option(
-        "-n", "--unit-name", default=None,
-        help="What should be used in place of the default unit. "
-        "Example: 1000 n instead of 1000 b or anything else.")
-    parser.add_option(
-        "-a", "--arguments", default=None,
-        help="Arguments for the plugin to be run. Not necessary "
-        "unless you're running a custom plugin. Given in the same "
-        "as you would call from the command line. "
-        "Example: -a '-w 10 -c 20 -f /usr/local'")
-    parser.add_option(
-        "-t", "--token", default=None,
-        help="The token for connecting.")
-    parser.add_option(
-        "-T", "--timeout", default=15, type="int",
-        help="Enforced timeout, will terminate plugins after "
-        "this amount of seconds. [%default]")
-    parser.add_option(
-        "-d", "--delta", action='store_true',
-        help="Signals that this check is a delta check and a "
-        "local state will kept.")
-    parser.add_option(
-        "-l", "--list", action='store_true',
-        help="List all values under a given node. Do not perform a check.")
-    parser.add_option(
-        "-v", "--verbose", action='store_true',
-        help='Print more verbose error messages.')
-    parser.add_option(
-        "-s", "--super-verbose", action='store_true',
-        help='Print LOTS of error messages.')
-    parser.add_option(
-        "-V", "--version", action='store_true',
-        help='Print version number of plugin.')
-    parser.add_option(
-        "-q", "--queryargs", default=None,
-        help='Extra query arguments to pass in the NCPA URL.')
+    parser.add_option("-H", "--hostname", help="The hostname to be connected to.")
+    parser.add_option("-M", "--metric", default='',
+                      help="The metric to check, this is defined on client "
+                           "system. This would also be the plugin name in the "
+                           "plugins directory. Do not attach arguments to it, "
+                           "use the -a directive for that. DO NOT INCLUDE the api/ "
+                           "instruction.")
+    parser.add_option("-P", "--port", default=5693, type="int",
+                      help="Port to use to connect to the client.")
+    parser.add_option("-w", "--warning", default=None, type="str",
+                      help="Warning value to be passed for the check.")
+    parser.add_option("-c", "--critical", default=None, type="str",
+                      help="Critical value to be passed for the check.")
+    parser.add_option("-u", "--unit", default=None,
+                      help="The unit prefix (M, G, T)")
+    parser.add_option("-n", "--units", default=None,
+                      help="What should be used in place of the default unit. As in, instead of 'b' as a unit, it will "
+                      "use this.")
+    parser.add_option("-a", "--arguments", default=None,
+                      help="Arguments for the plugin to be run. Not necessary "
+                           "unless you're running a custom plugin. Given in the same "
+                           "as you would call from the command line. Example: -a '-w 10 -c 20 -f /usr/local'")
+    parser.add_option("-t", "--token", default=None,
+                      help="The token for connecting.")
+    parser.add_option("-T", "--timeout", default=60, type="int",
+                      help="Enforced timeout, will terminate plugins after "
+                           "this amount of seconds. [%default]")
+    parser.add_option("-d", "--delta", action='store_true',
+                      help="Signals that this check is a delta check and a "
+                           "local state will kept.")
+    parser.add_option("-l", "--list", action='store_true',
+                      help="List all values under a given node. Do not perform "
+                           "a check.")
+    parser.add_option("-v", "--verbose", action='store_true',
+                      help='Print more verbose error messages.')
+    parser.add_option("-s", "--super-verbose", action='store_true',
+                      help='Print LOTS of error messages.')
+    parser.add_option("-V", "--version", action='store_true',
+                      help='Print version number of plugin.')
+    parser.add_option("-q", "--queryargs", default=None,
+                      help='Extra query arguments to pass in the NCPA URL.')
     options, _ = parser.parse_args()
 
     if options.version:
         print(version)
         sys.exit(0)
 
-    if options.arguments and options.metric \
-            and 'agent/plugin' not in options.metric:
+    if options.arguments and options.metric and not 'agent/plugin' in options.metric:
         parser.print_help()
         parser.error('You cannot specify arguments and try to run a plugin.')
 
@@ -145,9 +129,8 @@ def parse_args():
     return options
 
 
-# The following are all helper functions.
-# I would normally split these out into
-# a new module but this needs to be portable.
+# ~ The following are all helper functions. I would normally split these out into
+#~ a new module but this needs to be portable.
 
 
 def get_url_from_options(options):
@@ -164,7 +147,7 @@ def get_host_part_from_options(options):
     hostname = options.hostname
     port = options.port
 
-    if options.metric is not None:
+    if not options.metric is None:
         metric = options.metric
     else:
         metric = ''
@@ -181,9 +164,8 @@ def get_host_part_from_options(options):
 
 def get_check_arguments_from_options(options):
     """Gets the escaped URL for plugin arguments to be added
-    to the end of the host URL. This is different from the
-    get_arguments_from_options in that this is meant for the
-    syntax when the user is calling a check, whereas the below
+    to the end of the host URL. This is different from the get_arguments_from_options
+    in that this is meant for the syntax when the user is calling a check, whereas the below
     is when GET arguments need to be added.
 
     """
@@ -191,8 +173,7 @@ def get_check_arguments_from_options(options):
     if arguments is None:
         return ''
     else:
-        arguments = '/'.join(
-            [urlquote(x, safe='') for x in shlex.split(arguments)])
+        arguments = '/'.join([urlquote(x, safe='') for x in shlex.split(arguments)])
         return arguments
 
 
@@ -202,27 +183,25 @@ def get_arguments_from_options(options, **kwargs):
 
     """
 
-    # Note that the distinction between units/unit is a bit confusing.
-    # According to the API, the units the is the unit prefix,
-    # like K, M or G. Unit is the unit that will be ascribed
-    # like B, b, bytes, etc. We have them
+    # Note that the distinction between units/unit is a bit confusing. According to the API, the units the is
+    # the unit prefix, like K, M or G. Unit is the unit that will be ascribed like B, b, bytes, etc. We have them
     # flip-flopped here and cannot change them due to API versions.
     arguments = {'token': options.token,
-                 'units': options.units}
-
+                 'units': options.unit}
+    
     if not options.list:
         arguments['warning'] = options.warning
         arguments['critical'] = options.critical
         arguments['delta'] = options.delta
         arguments['check'] = 1
-        arguments['unit'] = options.unit_name
+        arguments['unit'] = options.units
 
     if options.queryargs:
         for argument in options.queryargs.split(','):
             key, value = argument.split('=')
             arguments[key] = value
 
-    # Encode the items in the dictionary that are not None
+    #~ Encode the items in the dictionary that are not None
     return urlencode(dict((k, v) for k, v in list(arguments.items()) if v))
 
 
@@ -236,26 +215,23 @@ def get_json(options):
     if options.verbose:
         print('Connecting to: ' + url)
 
-    try:
-        filename, _ = urlretrieve(url)
-        f = open(filename)
-    except IOError:
-        url = get_url_from_options(options)
-        filename, _ = urlretrieve(url)
-        f = open(filename)
+    ret = urlopen(url)
 
     if options.verbose:
-        print('File returned contained:\n' + ''.join(f.readlines()))
-        f.seek(0)
+        print('File returned contained:\n' + ''.join(ret))
 
-    return json.load(f)['value']
+    arr = json.load(ret)
+
+    if 'value' in arr:
+        return arr['value']
+
+    return arr
 
 
 def run_check(info_json):
-    '''
-    Run a check against the remote host.
-    '''
+    """Run a check against the remote host.
 
+    """
     return info_json['stdout'], info_json['returncode']
 
 
@@ -268,8 +244,7 @@ def show_list(info_json):
 
 def timeout_handler(threshold):
     def wrapped(signum, frames):
-        stdout = "UNKNOWN: Execution exceeded timeout threshold of %ds" % \
-            threshold
+        stdout = "UNKNOWN: Execution exceeded timeout threshold of %ds" % threshold
         print stdout
         sys.exit(3)
     return wrapped
@@ -300,8 +275,7 @@ def main():
         elif options.verbose:
             return 'An error occurred:' + str(e), 3
         else:
-            return 'UNKNOWN: Error occurred while running the plugin.' \
-                'Use the verbose flag for more details.', 3
+            return 'UNKNOWN: Error occurred while running the plugin. Use the verbose flag for more details.', 3
 
 
 if __name__ == "__main__":
