b3ca2c00a5dc57eab0f2c4d9c615ecd7e613a818
Jake Omann <jomann@nagios.com>
2016-05-25 23:19:16 -0500

    Add new scripts for installing pre-reqs

---
+++ build/scripts/linux_build_prereqs.sh
@@ -0,0 +1,29 @@
+if uname -a | grep -i 'Debian';
+then
+	apt-get install debian-builder rpm gcc git wget openssl libssl-dev libffi-dev zlib1g-dev alien -y
+elif uname -a | grep -i 'Darwin';
+then
+	echo "must be installed manually"
+elif uname -a | grep -i 'el5';
+then
+	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build wget -y
+elif  uname -a | grep -i 'el6';
+then
+	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build wget -y
+elif  uname -a | grep -i 'el7';
+then
+	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build wget -y
+else
+	echo "Could not determine your OS type... you need to install the following dependencies:"
+	echo ""
+	echo "gcc"
+	echo "zlib, zlib-devel"
+	echo "openssl, openssl-devel"
+	echo "libffi-devel"
+	echo "rpm-build"
+	echo "wget"
+	echo "git"
+	echo ""
+	echo "If you're running a debian distro you must also install debian-builder"
+	echo ""
+fi
---
+++ build/scripts/linux_build_setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Linux Build Server Setup Script
+# Creates a functioning build environment on a linux server
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHONTAR = Python-2.7.11
+PYTHONVER = python2.7
+CXFREEZEVER = cx_Freeze-4.3.4-patched
+CXLOGGINGVER = cx_Logging-2.1
+CXPYGENLIBVER = cx_PyGenLib-3.0
+PYTHONBIN = /usr/local/bin/python2.7
+
+# --------------------------
+#  INSTALL PRE-REQS
+# --------------------------
+./linux_build_prereqs.sh
+
+# --------------------------
+#  INSTALL RESOURCES
+# --------------------------
+
+# Install Python 
+cd ../resources
+tar xf $PYTHONTAR.tgz
+cd $PYTHONTAR && ./configure --with-zlib=/usr/include --enable-shared && make && make altinstall
+echo '/usr/local/lib' >> /etc/ld.so.conf 
+/sbin/ldconfig
+
+# Install cx_Freeze patched version
+cd ..
+tar xf $CXFREEZEVER.tar.gz
+
+# Install cx_Logging
+cd ..
+tar xf $CXLOGGINGVER.tar.gz
+
+# Install cx_PyGenLib
+cd ..
+tar xf $CXPYGENLIBVER.tar.gz
+
+# Clean up resource directory
+rm -rf $PYTHONTAR
+rm -rf $CXFREEZEVER
+rm -rf $CXLOGGINGVER
+rm -rf $CXPYGENLIBVER
+
+# --------------------------
+#  INSTALL PIP
+# --------------------------
+
+cd /tmp && wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py && $PYTHONBIN /tmp/get-pip.py
+
+# --------------------------
+#  INSTALL PIP COMPONENTS
+# --------------------------
+
+$PYTHONBIN -m pip install gevent gevent-websocket flask jinja2 requests pyOpenSSL sphinx docutils sphinx-bootstrap-theme psutil
+
+# --------------------------
+#  MISC SETUP
+# --------------------------
+
+# Add users if they don't exist
+useradd nagios
+groupadd nagcmd
+
+# Copy the autobuild script into root directory
+cp -f autobuild.sh /root
