e7cd18687f5bbc85bc15bf992b3213d9f52b0a39
Jake Omann <jomann@nagios.com>
2016-07-11 01:18:58 -0500

    Navbar no longer inverse

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ html_theme = 'bootstrap'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'navbar_class': 'navbar navbar-inverse'
+    'navbar_class': 'navbar'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
