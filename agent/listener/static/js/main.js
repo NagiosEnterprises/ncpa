21cbabb4aacf299ab7d0a29e736e54eb20a09d0e
Jake Omann <jomann@nagios.com>
2016-09-06 08:31:05 -0500

    Update API browser

--- agent/listener/static/js/main.js
+++ agent/listener/static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	// Default page content helpers
-	$('body').tooltip({ selector: '.tt-bind', container: 'body' });
+	$('body').tooltip({ html: true, selector: '.tt-bind', container: 'body' });
     $('body').popover({ html: true, selector: '.pop-bind', container: 'body' });
 
 });
