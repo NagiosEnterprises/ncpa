789b473c308deda9b0410f6189240c4ad75a34e2
Jake Omann <jomann@nagios.com>
2016-12-29 10:25:03 -0600

    Update include_files

--- agent/setup_posix.py
+++ agent/setup_posix.py
@@ -30,12 +30,12 @@ packages = []
 # Shared library include overrides
 bin_includes = ['libffi']
 
-includefiles += [('build_resources/LicenseAgreement.txt', 'build_resources/LicenseAgreement.txt'),
-                 ('build_resources/ncpa_listener.plist', 'build_resources/ncpa_listener.plist'),
-                 ('build_resources/ncpa_passive.plist', 'build_resources/ncpa_passive.plist'),
-                 ('build_resources/macosinstall.sh', 'build_resources/macosinstall.sh'),
-                 ('build_resources/listener_init', 'build_resources/listener_init'),
-                 ('build_resources/passive_init', 'build_resources/passive_init')]
+include_files += [('build_resources/LicenseAgreement.txt', 'build_resources/LicenseAgreement.txt'),
+                  ('build_resources/ncpa_listener.plist', 'build_resources/ncpa_listener.plist'),
+                  ('build_resources/ncpa_passive.plist', 'build_resources/ncpa_passive.plist'),
+                  ('build_resources/macosinstall.sh', 'build_resources/macosinstall.sh'),
+                  ('build_resources/listener_init', 'build_resources/listener_init'),
+                  ('build_resources/passive_init', 'build_resources/passive_init')]
 
 buildoptions = dict(includes=includes,
                     include_files=include_files,
