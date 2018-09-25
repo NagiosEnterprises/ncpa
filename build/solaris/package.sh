17216d1d592d4a1a52b49548ae30bcbacff97963
Jake Omann <jomann@nagios.com>
2018-09-25 17:29:03 -0500

    Solaris builds work... so far

--- build/solaris/package.sh
+++ build/solaris/package.sh
@@ -2,25 +2,26 @@
 
 # Get version
 DIR=$(dirname "$(readlink -f "$0")")
+BUILD_DIR=$(realpath "$DIR/..")
 VERSION=$(cat ../../VERSION)
 
 # Set up package info
 ARCH=$(arch)
-sed "s/ARCH=.*/ARCH=$ARCH/" pkginfo > pkginfo.tmp
-mv pkginfo.tmp ../pkginfo
+sed "s/ARCH=.*/ARCH=$ARCH/" $DIR/pkginfo > $BUILD_DIR/pkginfo
 
 # Make the package and cleanup
 (
-    cd ..
+    cd $BUILD_DIR
     cp pkginfo ncpa/pkginfo
 
     # Add prototype file
     echo 'i pkginfo' > prototype
-    pkgproto ncpa >> /prototype
+    pkgproto ncpa >> prototype
 
     # Build package and create the .pkg file
     pkgmk -b $(pwd) -o
     pkgtrans -s /var/spool/pkg ncpa-$VERSION.$ARCH.pkg ncpa
+    mv -f /var/spool/pkg/ncpa-$VERSION.$ARCH.pkg .
 
     # Remove build leftovers
     rm -rf /var/spool/pkg/ncpa
