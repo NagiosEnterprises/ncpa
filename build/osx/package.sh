b8604d8bc51c4b4869f135944e17d17ca8925c99
Jake Omann <jomann@nagios.com>
2018-10-09 10:14:41 -0500

    Update OS X builds with new folder names

--- build/osx/package.sh
+++ build/osx/package.sh
@@ -8,7 +8,7 @@ ARCH=$(arch)
 
 (
     mkdir NCPA-INSTALL-$VERSION
-    mv ncpa-$VERSION NCPA-INSTALL-$VERSION/ncpa
+    mv ncpa NCPA-INSTALL-$VERSION/ncpa
     cp NCPA-INSTALL-$VERSION/ncpa/build_resources/macosuninstall.sh NCPA-INSTALL-$VERSION/ncpa/uninstall.sh
     mv NCPA-INSTALL-$VERSION/ncpa/build_resources/macosinstall.sh NCPA-INSTALL-$VERSION/install.sh
     install_name_tool -change /Library/Frameworks/Python.framework/Versions/2.7/Python @executable_path/Python NCPA-INSTALL-$VERSION/ncpa/ncpa_listener
