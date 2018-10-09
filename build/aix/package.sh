e07dbee56d66ea0dbcec7060060fcf5aa1b723d1
Jake Omann <jomann@nagios.com>
2018-10-09 09:13:50 -0500

    Set up proper permissions in ncpa.spec files for AIX/Linux systems

--- build/aix/package.sh
+++ build/aix/package.sh
@@ -2,13 +2,13 @@
 
 # Set global build opts
 DIR=$(dirname "$(readlink -f "$0")")
-BUILD_DIR=$(realpath "$DIR/..")
+BUILD_DIR=$(readlink -f "$DIR/..")
 BUILD_RPM_DIR="/tmp/test/usr/src/redhat"
-VERSION=$(cat $BUILD_DIR/../VERSION)
+NCPA_VER=$(cat $BUILD_DIR/../VERSION)
 
 # Build spec file
 cd $BUILD_DIR
-cat linux/ncpa.spec | sed "s/__VERSION__/$VERSION/g" | sed "s|__BUILDROOT__|$BUILD_RPM_DIR|g" > $BUILD_DIR/ncpa.spec
+cat linux/ncpa.spec | sed "s/__VERSION__/$NCPA_VER/g" | sed "s|__BUILDROOT__|$BUILD_RPM_DIR|g" > $BUILD_DIR/ncpa.spec
 
 # Build rpm package (also used on Debian systems)
 (
@@ -17,9 +17,9 @@ cat linux/ncpa.spec | sed "s/__VERSION__/$VERSION/g" | sed "s|__BUILDROOT__|$BUI
     mkdir -p $BUILD_RPM_DIR/RPMS
     mkdir -p $BUILD_RPM_DIR/SOURCES
     mkdir -p $BUILD_RPM_DIR/BUILD
-    cp -f $BUILD_DIR/ncpa-$VERSION.tar.gz $BUILD_RPM_DIR/SOURCES/
+    cp ncpa-$NCPA_VER.tar.gz $BUILD_RPM_DIR/SOURCES/
     rm -f $BUILD_RPM_DIR/SPECS/ncpa.spec
-    cp -f $BUILD_DIR/ncpa.spec $BUILD_RPM_DIR/SPECS/
-    QA_RPATHS='$[ 0x0002 ]' rpmbuild $BUILD_RPM_DIR/SPECS/ncpa.spec -bb --define "_topdir $BUILD_RPM_DIR" >> $BUILD_DIR/build.log
-    find $BUILD_RPM_DIR/RPMS -name "ncpa-$VERSION*" -exec cp {} . \;
+    cp -f ncpa.spec $BUILD_RPM_DIR/SPECS/
+    rpmbuild -bb --define "_topdir $BUILD_RPM_DIR" $BUILD_RPM_DIR/SPECS/ncpa.spec
+    find $BUILD_RPM_DIR/RPMS -name "ncpa-$NCPA_VER*" -exec cp {} . \;
 )
