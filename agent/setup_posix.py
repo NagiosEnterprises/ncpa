4c131efe0442aeeea1181dc31c4c37dd28e2468c
Jake Omann <jomann@nagios.com>
2018-10-12 11:50:28 -0500

    Final fix for cffi backend libffi

--- agent/setup_posix.py
+++ agent/setup_posix.py
@@ -29,7 +29,7 @@ excludes = ['Tkinter','tkinter','collections.sys','collections._weakref']
 packages = []
 
 # Shared library include overrides
-bin_includes = ['libssl.so', 'libcrypto.so']
+bin_includes = ['libssl.so', 'libcrypto.so', 'libffi.so']
 
 # Special includes for AIX systems
 if 'aix' in sys.platform:
