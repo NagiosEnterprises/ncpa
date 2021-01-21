c49dd6fedbdd4bcc1e2ea43dae751f126e73e8fd
Jake Omann <jomann@nagios.com>
2021-01-20 20:17:56 -0600

    Fix issue with build

--- agent/listener/nodes.py
+++ agent/listener/nodes.py
@@ -8,6 +8,7 @@ import copy
 import re
 import listener.server
 import listener.database as database
+import ncpa
 
 
 # Valid nodes is updated as it gets set when calling a node via accessor
@@ -139,7 +140,7 @@ class RunnableParentNode(ParentNode):
             check_logging = 1
 
         # Send check results to database
-        if not listener.server.__INTERNAL__ and check_logging == 1:
+        if not ncpa.__INTERNAL__ and check_logging == 1:
             db = database.DB()
             current_time = time.time()
             db.add_check(kwargs['accessor'].rstrip('/'), current_time, current_time, primary_info['returncode'],
@@ -320,7 +321,7 @@ class RunnableNode(ParentNode):
             check_logging = 1
 
         # Send check results to database
-        if not child_check and not listener.server.__INTERNAL__ and check_logging == 1:
+        if not child_check and not ncpa.__INTERNAL__ and check_logging == 1:
             db = database.DB()
             current_time = time.time()
             db.add_check(kwargs['accessor'].rstrip('/'), current_time, current_time, returncode,
