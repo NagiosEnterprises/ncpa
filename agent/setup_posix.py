9ef1e7e9fce6d640dcfc94da116b7b912403910f
Phred White <pwhite@nagios.com>
2022-10-13 11:40:36 -0500

    Added Readme to files to be copied during build

--- agent/setup_posix.py
+++ agent/setup_posix.py
@@ -61,6 +61,7 @@ include_files += [('build_resources/LicenseAgreement.txt', 'build_resources/Lice
                   ('build_resources/ncpa_listener.plist', 'build_resources/ncpa_listener.plist'),
                   ('build_resources/ncpa_passive.plist', 'build_resources/ncpa_passive.plist'),
                   ('build_resources/macosinstall.sh', 'build_resources/macosinstall.sh'),
+                  ('build_resources/macosreadme.txt', 'build_resources/macosreadme.txt'),
                   ('build_resources/macosuninstall.sh', 'build_resources/macosuninstall.sh'),
                   ('build_resources/listener_init', 'build_resources/listener_init'),
                   ('build_resources/passive_init', 'build_resources/passive_init')]
