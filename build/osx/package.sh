a1c49a346f1cc96b58defaa058480effb029a1d5
Phred White <pwhite@nagios.com>
2022-12-13 18:07:15 +0000

    update mac packageer to move dylibs to correct location

--- build/osx/package.sh
+++ build/osx/package.sh
@@ -13,6 +13,7 @@ ARCH=$(arch)
     mv ncpa-$NCPA_VER NCPA-INSTALL-$NCPA_VER/ncpa
     cp NCPA-INSTALL-$NCPA_VER/ncpa/build_resources/macosuninstall.sh NCPA-INSTALL-$NCPA_VER/ncpa/uninstall.sh
     mv NCPA-INSTALL-$NCPA_VER/ncpa/build_resources/macosinstall.sh NCPA-INSTALL-$NCPA_VER/install.sh
+    mv NCPA-INSTALL-$NCPA_VER/ncpa/*.dylib NCPA-INSTALL-$NCPA_VER/lib
 #     install_name_tool -change /Library/Frameworks/Python.framework/Versions/2.7/Python @executable_path/Python NCPA-INSTALL-$NCPA_VER/ncpa/ncpa_listener
 #     install_name_tool -change /Library/Frameworks/Python.framework/Versions/2.7/Python @executable_path/Python NCPA-INSTALL-$NCPA_VER/ncpa/ncpa_passive
     hdiutil create -volname NCPA-$NCPA_VER -srcfolder NCPA-INSTALL-$NCPA_VER -ov -format UDZO ncpa_$NCPA_VER-1_amd64.dmg
