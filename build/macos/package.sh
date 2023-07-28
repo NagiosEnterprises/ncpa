4796b328d959520f0f39d16c6243af0e24a7e267
Phred White <pwhite@nagios.com>
2023-07-28 16:17:04 -0500

    Commented out some code used for debugging.

--- build/macos/package.sh
+++ build/macos/package.sh
@@ -16,8 +16,9 @@ ARCH=$(arch)
     install_name_tool -change /usr/local/opt/openssl@3/lib/libssl.3.dylib @loader_path/libssl.3.dylib ncpa-$NCPA_VER/lib/_ssl.cpython-311-darwin.so
     install_name_tool -change /usr/local/opt/openssl@3/lib/libcrypto.3.dylib @loader_path/libcrypto.3.dylib ncpa-$NCPA_VER/lib/_ssl.cpython-311-darwin.so
 
-    otool -L ncpa-$NCPA_VER/lib/libssl.3.dylib
-    otool -L ncpa-$NCPA_VER/lib/_ssl.cpython-311-darwin.so
+    # Uncomment otool comands to have updated dynamic lib dependencies dispayed
+    # otool -L ncpa-$NCPA_VER/lib/libssl.3.dylib
+    # otool -L ncpa-$NCPA_VER/lib/_ssl.cpython-311-darwin.so
 
     echo -e "\n***** macos/package.sh - copy other resources"
     mkdir NCPA-INSTALL-$NCPA_VER
@@ -27,7 +28,7 @@ ARCH=$(arch)
     mv NCPA-INSTALL-$NCPA_VER/ncpa/build_resources/macosinstall.sh NCPA-INSTALL-$NCPA_VER/install.sh
 
     # Create MacOS disk image file
-    echo -e "\n***** macos/package.sh - create .dmg file"
+    echo -e "\n***** macos/package.sh - creating .dmg file ..."
     RELEASE=$RELEASE"_"
     hdiutil create -volname NCPA-$NCPA_VER -srcfolder NCPA-INSTALL-$NCPA_VER -ov -format UDZO ncpa_$NCPA_VER-$RELEASE$ARCH.dmg
 )
