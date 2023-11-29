65cbfdf18cbc38ee9795463c3f8e87a0c0f7eda1
ne-bbahn <bbahnernagios@gmail.com>
2023-11-29 10:37:02 -0600

    updating linux scripts

--- build/linux/installers.sh
+++ build/linux/installers.sh
@@ -13,13 +13,13 @@ get_distro() {
         source /etc/os-release
         local myDistro="$ID$VERSION_ID "
     fi
-    echo $myDistro
+    echo "$myDistro"
 }
 
 # Returns OpenSSL version as an integer, e.g., v1.1.1f -> 111, v3.0.8 -> 308 for easy comparison
 get_sslver() {
     local ssl_ver=$(openssl version | grep -e "[1-3].[0-9].[0-9]" -o | head -n1 | sed 's/\.//g')
-    echo $ssl_ver
+    echo "$ssl_ver"
     return 0
 }
 
@@ -54,7 +54,7 @@ install_zlib() {
     local zLib_new_version="1.2.13"
 
     if [[ ! -z $1 ]]; then
-        zLib_new_version=$1
+        zLib_new_version="$1"
     else
         echo -e "ERROR! install_zlib() - No zLib version provided!"
         echo -e "********************************************\n\n"
@@ -64,19 +64,19 @@ install_zlib() {
     echo -e "Building zLib $zLib_new_version...\n"
 
     pushd /usr/src
-    wget https://zlib.net/zlib-$zLib_new_version.tar.gz --no-check-certificate
-    tar -zxf zlib-$zLib_new_version.tar.gz
+    wget "https://zlib.net/zlib-$zLib_new_version.tar.gz" --no-check-certificate
+    tar -zxf "zlib-$zLib_new_version.tar.gz"
 
-    pushd zlib-$zLib_new_version
+    pushd "zlib-$zLib_new_version"
     ./configure --64
     make && make test && make install
     popd
 
-    local libz=$(find "/usr" -name "libz.so*" | grep -v "/src" | grep $zLib_new_version | head -n 1)
+    local libz=$(find "/usr" -name "libz.so*" | grep -v "/src" | grep "$zLib_new_version" | head -n 1)
     echo -e "\n\n********************************************"
 
-    if [[ ! -z $libz ]]; then
-        rm -rf zlib-$zLib_new_version
+    if [[ ! -z "$libz" ]]; then
+        rm -rf "zlib-$zLib_new_version"
         echo -e "SUCCESS! zLib $zLib_new_version is installed @ $libz"
         echo -e "********************************************\n\n"
         return 0
@@ -94,8 +94,8 @@ install_openssl() {
     local ssl_new_path=""
     local ssl_new_base_path="/usr/local"
 
-    if [[ ! -z $1 ]]; then
-        ssl_new_version=$1
+    if [[ ! -z "$1" ]]; then
+        ssl_new_version="$1"
     else
         echo -e "ERROR! install_openssl() - No OpenSSL version provided!"
         echo -e "********************************************\n\n"
@@ -105,16 +105,16 @@ install_openssl() {
     local new_ver_int=$(echo "$ssl_new_version"  | sed 's/\.//g')
     # ssl_new_path="$ssl_new_base_path/ssl$new_ver_int"
     ssl_new_path="$ssl_new_base_path/openssl"
-    SSL_NEW_PATH=$ssl_new_path
+    SSL_NEW_PATH="$ssl_new_path"
 
     echo -e "\n\n********************************************"
     echo -e "Building OpenSSL $ssl_new_version at $ssl_new_path..."
     echo -e " "
 
-    local ssl_old_path=$(dirname $(dirname $(which openssl)))
-    local oldlib=$(find /usr -name "libssl.so*" | grep -v /src | head -n1)
+    local ssl_old_path="$(dirname $(dirname $(which openssl)))"
+    local oldlib="$(find /usr -name "libssl.so*" | grep -v /src | head -n1)"
     if [[ -z "$oldlib" ]]; then
-        oldlib=$(find /lib -name "libssl.so*" | grep -v /src | head -n1)
+        oldlib="$(find /lib -name "libssl.so*" | grep -v /src | head -n1)"
     fi
     local ssl_old_lib=$(dirname $oldlib)
     local my_distro=$(get_distro)
@@ -123,39 +123,39 @@ install_openssl() {
 
     pushd /usr/src
 
-    wget https://www.openssl.org/source/openssl-$ssl_new_version.tar.gz --no-check-certificate
-    tar -zxf openssl-$ssl_new_version.tar.gz
+    wget "https://www.openssl.org/source/openssl-$ssl_new_version.tar.gz" --no-check-certificate
+    tar -zxf "openssl-$ssl_new_version.tar.gz"
 
-    pushd openssl-$ssl_new_version
-    ./config --prefix=$ssl_new_path
+    pushd "openssl-$ssl_new_version"
+    ./config --prefix="$ssl_new_path"
     make all && make test && make install
     popd
 
     #If lib path hasn't changed then we don't need to link the new libraries. TODO confirm this.
     if [[ "$ssl_old_lib" != "$ssl_new_path/lib64" ]]; then
         if [[ -f "$ssl_old_lib/libssl.so" ]]; then
-            mv $ssl_old_lib/libssl.so $ssl_old_lib/libssl.so.bak
-            mv $ssl_old_lib/libcrypto.so $ssl_old_lib/libcrypto.so.bak
+            mv "$ssl_old_lib/libssl.so" "$ssl_old_lib/libssl.so.bak"
+            mv "$ssl_old_lib/libcrypto.so" "$ssl_old_lib/libcrypto.so.bak"
         fi
-        ln -s $ssl_new_path/lib64/libssl.so $ssl_old_lib/libssl.so
-        ln -s $ssl_new_path/lib64/libcrypto.so $ssl_old_lib/libcrypto.so
+        ln -s "$ssl_new_path/lib64/libssl.so" "$ssl_old_lib/libssl.so"
+        ln -s "$ssl_new_path/lib64/libcrypto.so" "$ssl_old_lib/libcrypto.so"
 
         if [[ -f "$ssl_old_lib/libssl.so.3" ]]; then
-            mv $ssl_old_lib/libssl.so.3 $ssl_old_lib/libssl.so.3.bak
-            mv $ssl_old_lib/libcrypto.so.3 $ssl_old_lib/libcrypto.so.3.bak
+            mv "$ssl_old_lib/libssl.so.3" "$ssl_old_lib/libssl.so.3.bak"
+            mv "$ssl_old_lib/libcrypto.so.3" "$ssl_old_lib/libcrypto.so.3.bak"
         fi
-        ln -s $ssl_new_path/lib64/libssl.so.3 $ssl_old_lib/libssl.so.3
-        ln -s $ssl_new_path/lib64/libcrypto.so.3 $ssl_old_lib/libcrypto.so.3
+        ln -s "$ssl_new_path/lib64/libssl.so.3" "$ssl_old_lib/libssl.so.3"
+        ln -s "$ssl_new_path/lib64/libcrypto.so.3" "$ssl_old_lib/libcrypto.so.3"
     fi
 
     echo -e "\nlibssl under /usr:"
     find "/usr" -name "libssl.so*" | grep -v /src
     echo -e "\nold lib ($ssl_old_lib) links :"
-    ls -al $ssl_old_lib | grep -E "lib(ssl|crypto)"
+    ls -al "$ssl_old_lib" | grep -E "lib(ssl|crypto)"
 
     popd
 
-    local sslchk=$($ssl_new_path/bin/openssl version | grep $ssl_new_version)
+    local sslchk="$("$ssl_new_path/bin/openssl" version | grep "$ssl_new_version")"
     echo -e "\nsslchk: $sslchk"
     echo -e "\n\n********************************************"
 
@@ -164,7 +164,7 @@ install_openssl() {
         PATH="$ssl_new_path/bin:$PATH"
         echo -e "After PATH: $PATH"
 
-        rm -rf openssl-$ssl_new_version
+        rm -rf "openssl-$ssl_new_version"
         echo -e "SUCCESS! OpenSSL $ssl_new_version is installed"
         echo -e "********************************************\n\n"
         return 0
@@ -181,11 +181,11 @@ install_python() {
     local python_new_version=""
     local ssl_new_path=""
 
-    if [[ ! -z $1 ]]; then
-        python_new_version=$1
+    if [[ ! -z "$1" ]]; then
+        python_new_version="$1"
     else
-        if [[ ! -z $PYTHONVER ]]; then
-            python_new_version=$PYTHONVER
+        if [[ ! -z "$PYTHONVER" ]]; then
+            python_new_version="$PYTHONVER"
         else
             echo -e "ERROR! install_python() - No Python version provided!"
             echo -e "********************************************\n\n"
@@ -208,20 +208,20 @@ install_python() {
 
     local pythontar="Python-$python_new_version"
 
-    if [ ! -f $pythontar.tgz ]; then
-        wget https://www.python.org/ftp/python/$python_new_version/$pythontar.tgz
+    if [ ! -f "$pythontar.tgz" ]; then
+        wget "https://www.python.org/ftp/python/$python_new_version/$pythontar.tgz"
     fi
-    tar xf $pythontar.tgz
-    cd $pythontar
+    tar xf "$pythontar.tgz"
+    cd "$pythontar"
     echo -e " "
-    if [[ -z $ssl_new_path ]]; then
+    if [[ -z "$ssl_new_path" ]]; then
         echo -e "Configure Python with default OpenSSL path"
         ./configure && \
         make && \
         make altinstall
     else
         echo -e "Configure Python with custom OpenSSL path: $ssl_new_path"
-        ./configure  --with-openssl=$ssl_new_path && \
+        ./configure  --with-openssl="$ssl_new_path" && \
         make && \
         make altinstall
     fi
@@ -235,11 +235,11 @@ install_ssl_and_zlib() {
     local ssl_new_version=""
     local zLib_new_version=""
 
-    if [[ ! -z $1 ]]; then
-        ssl_new_version=$1
+    if [[ ! -z "$1" ]]; then
+        ssl_new_version="$1"
     else
-        if [[ ! -z $SSLVER ]]; then
-            ssl_new_version=$SSLVER
+        if [[ ! -z "$SSLVER" ]]; then
+            ssl_new_version="$SSLVER"
         else
             echo -e "ERROR! install_ssl_and_zlib() - No OpenSSL version provided!"
             echo -e "********************************************\n\n"
@@ -247,11 +247,11 @@ install_ssl_and_zlib() {
         fi
     fi
 
-    if [[ ! -z $2 ]]; then
-        zLib_new_version=$2
+    if [[ ! -z "$2" ]]; then
+        zLib_new_version="$2"
     else
-        if [[ ! -z $ZLIBVER ]]; then
-            zLib_new_version=$ZLIBVER
+        if [[ ! -z "$ZLIBVER" ]]; then
+            zLib_new_version="$ZLIBVER"
         else
             echo -e "ERROR! install_ssl_and_zlib() - No zLib version provided!"
             echo -e "********************************************\n\n"
@@ -259,15 +259,15 @@ install_ssl_and_zlib() {
         fi
     fi
 
-    curr_ver_int=$(get_sslver)
-    new_ver_int=$(echo "$ssl_new_version"  | sed 's/\.//g')
+    curr_ver_int="$(get_sslver)"
+    new_ver_int="$(echo "$ssl_new_version"  | sed 's/\.//g')"
 
     if (( "$new_ver_int" > "$curr_ver_int" )); then
         echo -e "Updating zLib and OpenSSL..."
 
         install_devtools && \
-        install_zlib $zLib_new_version && \
-        install_openssl $ssl_new_version
+        install_zlib "$zLib_new_version" && \
+        install_openssl "$ssl_new_version"
 
         return 0
     else
@@ -279,18 +279,18 @@ install_ssl_and_zlib() {
 # Requires globals $PYTHONBIN and $BUILD_DIR
 update_py_packages() {
     echo -e "***** linux/installers.sh - update_py_packages()"
-    $PYTHONBIN -m pip install --upgrade pip
-    $PYTHONBIN -m pip install -r $BUILD_DIR/resources/require.txt --upgrade
+    "$PYTHONBIN" -m pip install --upgrade pip
+    "$PYTHONBIN" -m pip install -r "$BUILD_DIR"/resources/require.txt --upgrade
 
     # cx freeze doesn't grab the proper _sslxxx.so and other libs, so we copy in the real ones.
-    pylibpath=$(echo $PYTHONBIN | sed 's/bin/lib/g')
+    pylibpath="$(echo "$PYTHONBIN" | sed 's/bin/lib/g')"
     echo -e "***** linux/installers.sh - update_py_packages() - copy $pylibpath/lib-dynload to cx_freeze lib-dynload"
 
     if [ ! -d "$pylibpath/site-packages/cx_Freeze/bases/lib-dynload_orig" ]; then
-        mkdir $pylibpath/site-packages/cx_Freeze/bases/lib-dynload_orig
+        mkdir "$pylibpath/site-packages/cx_Freeze/bases/lib-dynload_orig"
     fi
-    cp $pylibpath/site-packages/cx_Freeze/bases/lib-dynload/* $pylibpath/site-packages/cx_Freeze/bases/lib-dynload_orig/
+    cp "$pylibpath/site-packages/cx_Freeze/bases/lib-dynload/"* "$pylibpath/site-packages/cx_Freeze/bases/lib-dynload_orig/"
 
     # Link python's lib-dynload to cx_freeze lib-dynload to make sure we are using desired OpenSSL, etc.
-    cp $pylibpath/lib-dynload/* $pylibpath/site-packages/cx_Freeze/bases/lib-dynload/
+    cp "$pylibpath/lib-dynload/"* "$pylibpath/site-packages/cx_Freeze/bases/lib-dynload/"
 }
--- build/linux/package.sh
+++ build/linux/package.sh
@@ -3,46 +3,46 @@
 echo -e "***** linux/package.sh"
 
 # Set global build opts
-DIR=$(dirname "$(readlink -f "$0")")
-BUILD_DIR=$(readlink -f "$DIR/..")
+DIR="$(dirname "$(readlink -f "$0")")"
+BUILD_DIR="$(readlink -f "$DIR/..")"
 BUILD_RPM_DIR="/usr/src/redhat"
 
 # Get information about system
-. $BUILD_DIR/linux/init.sh
-NCPA_VER=$(cat $BUILD_DIR/../VERSION)
+. "$BUILD_DIR"/linux/init.sh
+NCPA_VER="$(cat "$BUILD_DIR"/../VERSION)"
 
 # Build spec file
 echo -e "***** Build spec file"
-cd $BUILD_DIR
-cat linux/ncpa.spec | sed "s/__VERSION__/$NCPA_VER/g" | sed "s|__BUILDROOT__|$BUILD_RPM_DIR|g" > $BUILD_DIR/ncpa.spec
+cd "$BUILD_DIR"
+cat linux/ncpa.spec | sed "s/__VERSION__/"$NCPA_VER"/g" | sed "s|__BUILDROOT__|$BUILD_RPM_DIR|g" > "$BUILD_DIR"/ncpa.spec
 
 # Build rpm package (also used on Debian systems)
 echo -e "***** Build rpm package"
 (
-    if [[ -d $BUILD_RPM_DIR ]]; then
+    if [[ -d "$BUILD_RPM_DIR" ]]; then
         echo -e "***** Remove old rpm package directories"
-        rm -r $BUILD_RPM_DIR
+        rm -r "$BUILD_RPM_DIR"
     fi
     echo -e "***** Build rpm package - make directories"
-    mkdir -p $BUILD_RPM_DIR/SPECS
-    mkdir -p $BUILD_RPM_DIR/SRPMS
-    mkdir -p $BUILD_RPM_DIR/RPMS
-    mkdir -p $BUILD_RPM_DIR/SOURCES
-    mkdir -p $BUILD_RPM_DIR/BUILD
-    cp -f $BUILD_DIR/ncpa-$NCPA_VER.tar.gz $BUILD_RPM_DIR/SOURCES/
-    rm -f $BUILD_RPM_DIR/SPECS/ncpa.spec
-    cp -f $BUILD_DIR/ncpa.spec $BUILD_RPM_DIR/SPECS/
+    mkdir -p "$BUILD_RPM_DIR"/SPECS
+    mkdir -p "$BUILD_RPM_DIR"/SRPMS
+    mkdir -p "$BUILD_RPM_DIR"/RPMS
+    mkdir -p "$BUILD_RPM_DIR"/SOURCES
+    mkdir -p "$BUILD_RPM_DIR"/BUILD
+    cp -f "$BUILD_DIR"/ncpa-"$NCPA_VER".tar.gz "$BUILD_RPM_DIR"/SOURCES/
+    rm -f "$BUILD_RPM_DIR"/SPECS/ncpa.spec
+    cp -f "$BUILD_DIR"/ncpa.spec "$BUILD_RPM_DIR"/SPECS/
 
     echo -e "***** Build rpm package - rpmbuild"
     if [ "$distro" == "Raspbian" ]; then
         parch=`uname -m`
-        QA_RPATHS='$[ 0x0002 ]' rpmbuild $BUILD_RPM_DIR/SPECS/ncpa.spec -bb --target=armhf --define "_topdir $BUILD_RPM_DIR" --define "_arch armhf" >> $BUILD_DIR/build.log
+        QA_RPATHS='$[ 0x0002 ]' rpmbuild "$BUILD_RPM_DIR"/SPECS/ncpa.spec -bb --target=armhf --define "_topdir $BUILD_RPM_DIR" --define "_arch armhf" >> "$BUILD_DIR"/build.log
     else
-        QA_RPATHS='$[ 0x0002 ]' rpmbuild $BUILD_RPM_DIR/SPECS/ncpa.spec -bb --define "_topdir $BUILD_RPM_DIR" >> $BUILD_DIR/build.log
+        QA_RPATHS='$[ 0x0002 ]' rpmbuild "$BUILD_RPM_DIR"/SPECS/ncpa.spec -bb --define "_topdir $BUILD_RPM_DIR" >> "$BUILD_DIR"/build.log
     fi
 
     echo -e "***** Build rpm package - find RPMs and copy to build dir"
-    find $BUILD_RPM_DIR/RPMS -name "ncpa-$NCPA_VER*" -exec cp {} . \;
+    find "$BUILD_RPM_DIR"/RPMS -name "ncpa-$NCPA_VER*" -exec cp {} . \;
 )
 
 dist_ver="notcentos7"
@@ -65,7 +65,7 @@ if [ "$distro" == "Debian" ] || [ "$distro" == "Ubuntu" ] || [ "$distro" == "Ras
     fi
 
     echo -e "***** Convert to .deb - mk debbuild dir"
-    cd $BUILD_DIR
+    cd "$BUILD_DIR"
     mkdir -p debbuild
     cp *.rpm debbuild/
     ls -al
@@ -79,13 +79,13 @@ if [ "$distro" == "Debian" ] || [ "$distro" == "Ubuntu" ] || [ "$distro" == "Ras
 
     echo -e "***** Convert to .deb - run alien"
     if [ "$architecture" == "aarch64" ]; then
-      alien -c -k -v --target=arm64 $rpm >> $BUILD_DIR/build.log
+      alien -c -k -v --target=arm64 "$rpm" >> "$BUILD_DIR"/build.log
     else
-      alien -c -k -v $rpm >> $BUILD_DIR/build.log
+      alien -c -k -v "$rpm" >> "$BUILD_DIR"/build.log
     fi
     echo -e "***** Convert to .deb - alien done"
 
-    cd $BUILD_DIR
+    cd "$BUILD_DIR"
     cp debbuild/*.deb .
     cp debbuild/*.rpm .
 
