961d52f2258c57fa041323cd03df8a4115d853d5
ne-bbahn <bbahnernagios@gmail.com>
2023-11-30 15:35:48 -0600

    update ncpa.py __VERSION__ for 3.0.1

--- agent/ncpa.py
+++ agent/ncpa.py
@@ -69,7 +69,7 @@ if os.name == 'nt':
 
 # Set some global variables for later
 __FROZEN__ = getattr(sys, 'frozen', False)
-__VERSION__ = '3.0.0'
+__VERSION__ = '3.0.1'
 __DEBUG__ = False
 __SYSTEM__ = os.name
 __STARTED__ = datetime.datetime.now()
