8186741308d99f958d60e30c5d5b5d6b6f5c525a
ne-bbahn <bbahnernagios@gmail.com>
2024-04-11 10:28:13 -0500

    testing

--- agent/listener/server.py
+++ agent/listener/server.py
@@ -1178,10 +1178,16 @@ def write_to_configFile(section, option, value):
                     logging.debug("write_to_configFile() - section: %s", section)
                     continue
                 for (target_section, tbl_option, option_in_file) in allowed_modifications_tuples:
-                    logging.info("\n\n\n write_to_configFile() - target_section: %s", target_section)
+                    logging.info("\n\n write_to_configFile() - target_section: %s", target_section)
                     logging.info("write_to_configFile() - option_in_file: %s", option_in_file)
                     logging.info("write_to_configFile() - tbl_option: %s", tbl_option)
                     logging.info("write_to_configFile() - option: %s", option)
+                    if section == target_section:
+                        logging.info("match section")
+                    if option == tbl_option:
+                        logging.info("match option")
+                    if line.startswith(option_in_file + " ="):
+                        logging.info("match option_in_file")
                     if section == target_section and option == tbl_option and line.startswith(option_in_file + " ="):
                         logging.info("write_to_configFile() - line: %s", line)
                         sed_cmd = f"sed -i '{i}s/.*/{option_in_file} = {value}/' {cfg_file}"
