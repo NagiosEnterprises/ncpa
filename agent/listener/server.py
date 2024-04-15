3730c4b6c7c4f0aaf453a2eba25baad9bee5f4c0
ne-bbahn <bbahnernagios@gmail.com>
2024-04-15 17:34:59 -0500

    removing non-functional testing

--- agent/listener/server.py
+++ agent/listener/server.py
@@ -19,7 +19,6 @@ import urllib.parse
 import gevent
 import ncpa
 from ncpa import listener_logger as logging
-from ncpa import d as daemon
 #import inspect
 
 
@@ -1193,7 +1192,6 @@ def write_to_config_and_file(section_option_value_dict):
 @listener.route('/update-config/', methods=['POST'], provide_automatic_options = False)
 @requires_admin_auth
 def set_config(section=None):
-    logging.info("start_service() - daemon.l, daemon.p: %s, %s", daemon.l, daemon.p)
     config = listener.config['iconfig']
     if config.get('listener', 'allow_config_edit') != '1':
         return jsonify({'error': 'Editing the config is disabled.'})
