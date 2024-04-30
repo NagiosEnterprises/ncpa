6b3b05db9e140bcfeec115c0c8b724ecd146b359
ne-bbahn <bbahnernagios@gmail.com>
2024-04-30 16:38:33 -0500

    trying to fix this...

--- agent/listener/server.py
+++ agent/listener/server.py
@@ -1201,7 +1201,7 @@ def write_to_config_and_file(section_options_to_update):
                     logging.error("File not found: %s", cfg_file)
                     return
                 
-                lines[line_number-1] = lines[line_number-1].replace(old_value, new_value)
+                lines[line_number-1] = new_value + '\n'
                 logging.debug("write_to_configFile() - attempting to replace line number %s with %s", line_number, new_value)
 
                 # try:
