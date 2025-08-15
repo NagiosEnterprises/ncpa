d3fa911c833f49f4f8c4bc03a93108347f679c4d
ne-bbahn <bbahnernagios@gmail.com>
2025-08-15 15:15:49 -0500

    removing artifacts from previous install

--- build/macos/installers.sh
+++ build/macos/installers.sh
@@ -56,6 +56,12 @@ os_minor_version=$(echo $os_version | cut -f2 -d.)
 clean_necessary_files() {
     echo "    - Removing conflicting Homebrew libraries: readline, sqlite, expat..."
     sudo rm -rf /usr/local/opt/readline /usr/local/opt/sqlite /usr/local/opt/expat
+    echo "    - Autoremoving Homebrew Python, OpenSSL, and ca-certificates to prevent SSL issues..."
+    brew uninstall --ignore-dependencies python@3.13 || true
+    sudo rm -rf /usr/local/Cellar/python@3.13/
+    brew uninstall --ignore-dependencies openssl@3 || true
+    sudo rm -rf /usr/local/etc/ca-certificates/
+    sudo rm ./prereqs.installed
 }
 clean_necessary_files;
 
