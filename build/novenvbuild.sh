9269df3cc8963980e6ebf4d05e382771d4abf593
ne-bbahn <bbahnernagios@gmail.com>
2025-08-15 09:52:20 -0500

    removed non-Mac things from novenvbuild.sh since we only use that for Mac

--- build/novenvbuild.sh
+++ build/novenvbuild.sh
@@ -7,13 +7,8 @@ BUILD_DIR_FOR_VERSION=$( cd "$(dirname "$0")" ; pwd -P )
 source "$BUILD_DIR_FOR_VERSION/version_config.sh"
 
 UNAME=$(uname)
-if [ "$UNAME" == "Darwin" ] || [ "$UNAME" == "AIX" ] || [ "$UNAME" == "SunOS" ]; then
-    BUILD_DIR=$( cd "$(dirname "$0")" ; pwd -P )
-    AGENT_DIR="$BUILD_DIR/../agent"
-else
-    BUILD_DIR=$(dirname "$(readlink -f "$0")")
-    AGENT_DIR=$(readlink -f "$BUILD_DIR/../agent")
-fi
+BUILD_DIR=$( cd "$(dirname "$0")" ; pwd -P )
+AGENT_DIR="$BUILD_DIR/../agent"
 NCPA_VER=$(cat $BUILD_DIR/../VERSION)
 
 # User-defined variables
@@ -30,8 +25,7 @@ NO_INTERACTION=0
 
 
 usage() {
-    echo "Use the build.sh script to setup build environment, compile, "
-    echo "and package builds. Works with most common linux OS."
+    echo "Use the build.sh script to setup build environment, compile, and package builds for MacOS/Darwin."
     echo ""
     echo "Example: ./build.sh"
     echo ""
@@ -42,26 +36,16 @@ usage() {
     echo "  -p | --package-only Bundle a package only (ncpa folder must exist"
     echo "                      in the build directory)"
     echo "  -b | --build-only   Build the ncpa binaries only (do not package)"
-    echo "  -T | --travis       Set up environment for Travis CI builds"
     echo "  -c | --clean        Clean up the build directory"
     echo "  -n | --no-interaction  Run without interactive prompts (auto-confirm)"
     echo ""
-    echo "Operating Systems Supported:"
-    echo " - CentOS, RHEL, Oracle, CloudLinux"
-    echo " - Ubuntu, Debian"
-    echo " - OpenSUSE, SLES"
-    echo " - AIX *"
-    echo " - Solaris *"
-    echo ""
-    echo "* Some systems require extra initial setup, find out more:"
-    echo "https://github.com/NagiosEnterprises/ncpa/blob/master/BUILDING.rst"
+    echo "Operating System Supported: MacOS/Darwin only."
     echo ""
 }
 
 
 clean_build_dir() {
     echo -e "\n***** build/build.sh - Cleaning up build directory..."
-    # Remove directories named ncpa-* except for the current version, but do not delete .rpm files
     for dir in "$BUILD_DIR"/ncpa-*; do
         if [ -d "$dir" ]; then
             sudo rm -rf "$dir"
@@ -69,11 +53,9 @@ clean_build_dir() {
     done
     sudo rm -rf $AGENT_DIR/build
     sudo rm -rf $BUILD_DIR/NCPA-INSTALL-*
-    # sudo rm -f $BUILD_DIR/*.rpm $BUILD_DIR/*.dmg $BUILD_DIR/*.deb
     sudo rm -f $BUILD_DIR/ncpa.spec
     sudo rm -f $BUILD_DIR/*.tar.gz
     sudo rm -rf $BUILD_ROOT
-    sudo rm -rf $BUILD_DIR/debbuild
 }
 
 
@@ -120,25 +102,15 @@ done
 # --------------------------
 
 
-# Load required things for different systems
 echo -e "\nRunning build for: $UNAME"
-if [ "$UNAME" == "Linux" ]; then
-    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$PATH
-    . $BUILD_DIR/linux/setup.sh
-elif [ "$UNAME" == "SunOS" ] || [ "$UNAME" == "Solaris" ]; then
-    . $BUILD_DIR/solaris/setup.sh
-elif [ "$UNAME" == "AIX" ]; then
-    . $BUILD_DIR/aix/setup.sh
-elif [ "$UNAME" == "Darwin" ]; then
+if [ "$UNAME" == "Darwin" ]; then
     . $BUILD_DIR/macos/setup.sh
 else
     echo "Not a supported system for our build script."
-    echo "If you're sure all pre-reqs are installed, try running the"
-    echo "build without setup: ./build.sh --build-only"
+    exit 1
 fi
 
-# Check that pre-reqs have been installed
-if [ $BUILD_TRAVIS -eq 0 ] && [ $PACKAGE_ONLY -eq 0 ] && [ $BUILD_ONLY -eq 0 ]; then
+if [ $PACKAGE_ONLY -eq 0 ] && [ $BUILD_ONLY -eq 0 ]; then
     if { [ ! -f $BUILD_DIR/prereqs.installed ] && [ $SKIP_SETUP -eq 0 ]; } || ! which $PYTHONBIN > /dev/null; then
         echo "** WARNING: This should not be done on a production system. **"
         if [ $NO_INTERACTION -eq 1 ] || { read -r -p "Automatically install system pre-reqs? [Y/n] " resp && [[ $resp =~ ^(yes|y|Y| ) ]] || [[ -z $resp ]]; }; then
@@ -146,13 +118,6 @@ if [ $BUILD_TRAVIS -eq 0 ] && [ $PACKAGE_ONLY -eq 0 ] && [ $BUILD_ONLY -eq 0 ]; 
             sudo touch $BUILD_DIR/prereqs.installed
         fi
     fi
-
-elif [ $BUILD_TRAVIS -eq 1 ]; then
-    # Set up travis environment
-    sudo useradd nagios
-    cd $BUILD_DIR
-    python -m pip install -r resources/require.txt --upgrade
-    exit 0
 fi
 
 
@@ -226,13 +191,8 @@ fi
     echo "UNAME: $UNAME"
     
     # Copy build directory with platform-specific handling for symbolic links
-    if [ "$UNAME" == "Darwin" ]; then
-        # On macOS, use -L to follow symbolic links to avoid issues with relative paths
-        sudo cp -RLf "$BUILD_EXE_DIR" $BUILD_DIR/ncpa
-    else
-        # On other systems, use standard recursive copy
-        sudo cp -rf "$BUILD_EXE_DIR" $BUILD_DIR/ncpa
-    fi
+    # On macOS, use -L to follow symbolic links to avoid issues with relative paths
+    sudo cp -RLf "$BUILD_EXE_DIR" $BUILD_DIR/ncpa
     
     echo $GIT_LONG | sudo tee $BUILD_DIR/ncpa/$GIT_HASH_FILE
 
@@ -256,14 +216,8 @@ fi
     # Build tarball
     echo -e "\nBuilding tarball..."
     sudo cp -rf ncpa ncpa-$NCPA_VER
-    if [ "$UNAME" == "AIX" ]; then
-        echo -e "***** Build tarball"
-        sudo tar cvf ncpa-$NCPA_VER.tar ncpa-$NCPA_VER | sudo tee -a $BUILD_DIR/build.log
-        sudo gzip -f ncpa-$NCPA_VER.tar | sudo tee -a $BUILD_DIR/build.log
-    elif [ "$UNAME" == "Linux" ]; then
-        echo -e "***** Build tarball"
-        sudo tar -czvf ncpa-$NCPA_VER.tar.gz ncpa-$NCPA_VER | sudo tee -a $BUILD_DIR/build.log
-    fi
+    echo -e "***** Build tarball"
+    sudo tar -czvf ncpa-$NCPA_VER.tar.gz ncpa-$NCPA_VER | sudo tee -a $BUILD_DIR/build.log
 )
 
 
@@ -273,27 +227,9 @@ fi
 
 
 if [ $BUILD_ONLY -eq 0 ]; then
-
-    # Build package based on system
-    echo -e "\nPackaging for system type..."
-
-    if [ "$UNAME" == "Linux" ]; then
-        linux/package.sh
-    elif [ "$UNAME" == "SunOS" ] || [ "$UNAME" == "Solaris" ]; then
-        solaris/package.sh
-    elif [ "$UNAME" == "AIX" ]; then
-        aix/package.sh
-    elif [ "$UNAME" == "Darwin" ]; then
-        macos/package.sh
-    else
-        echo "No packaging method exists. You can locate binaries here:"
-        echo "$BUILD_DIR/ncpa"
-    fi
-
-    # Remove the build directory and tar.gz
+    echo -e "\nPackaging for MacOS..."
+    macos/package.sh
     echo -e "\nClean up packaging dirs..."
     cd $BUILD_DIR
     sudo rm -rf *.tar.gz
-    # sudo rm -rf ncpa-$NCPA_VER
-
 fi
