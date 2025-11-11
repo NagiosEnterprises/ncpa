e71d47e32fb449f82ca54908dc5d658e876a20b8
Coleman Dietsch <ne.cdietsch@gmail.com>
2025-11-11 12:36:24 -0800

    fix: prevent duplicate log entries from appearing in the listener log file

--- agent/ncpa.py
+++ agent/ncpa.py
@@ -248,6 +248,7 @@ class Listener(Base):
     def run(self):
         self.init_logger('listener')
         logger = self.logger
+        logger.info("Starting Listener")
         logger.info("run()")
 
         try:
@@ -387,6 +388,7 @@ class Passive(Base):
     def run(self):
         self.init_logger('passive')
         logger = self.logger
+        logger.info("Starting Passive")
         logger.info("run()")
 
         # Check if there is a start delay
@@ -962,10 +964,6 @@ if __SYSTEM__ == 'nt':
 
             self.setup_plugins()
             self.logger.debug("Looking for plugins at: %s" % self.abs_plugin_path)
-
-            self.init_logger('listener')
-            for handler in self.logger.handlers:
-                handler.addFilter(tokenFilter)
 
 
         def init_logger(self, logger_name):
