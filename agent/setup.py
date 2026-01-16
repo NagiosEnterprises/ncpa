63603dd81640ab55fb1b95630378333c1a5b2a35
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-16 09:25:57 -0800

    change base back to original setting

--- agent/setup.py
+++ agent/setup.py
@@ -109,9 +109,7 @@ elif __SYSTEM__ == 'posix':
     if 'sunos' in sys.platform.lower():
         include_files += get_solaris_lib_paths()
 
-    binary = Executable('ncpa.py', base='console')
-
-print("Building NCPA version ", version)
+    binary = Executable('ncpa.py', base=None)
 
 # Apply build options
 buildOptions = dict(includes=includes,
