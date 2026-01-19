75fefde30c130d52afc1bf73007aa11eb3552296
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-19 14:45:47 -0800

    trying different syntax for build binary

--- agent/setup.py
+++ agent/setup.py
@@ -110,7 +110,8 @@ elif __SYSTEM__ == 'posix':
     if 'sunos' in sys.platform.lower():
         include_files += get_solaris_lib_paths()
 
-    binary = Executable('ncpa.py', base=None)
+    # binary = Executable('ncpa.py', base=None)
+    binary = {"script": "ncpa.py", "base": "console"}
 
 # Apply build options
 buildOptions = dict(includes=includes,
