dc7e3d72af2495f5d75d5ce5cb360806ca55f708
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-21 09:01:53 -0800

    update include files

--- agent/setup.py
+++ agent/setup.py
@@ -101,6 +101,9 @@ elif __SYSTEM__ == 'posix':
     if 'aix' in sys.platform:
         include_files += [('/opt/freeware/lib/libpython3.12.a'  , 'build_resources/libpython3.12.a'),
                           ('/usr/lib/libsqlite3.a'              , 'build_resources/libsqlite3.a'),
+                          ('/usr/lib/libssl.a'                  , 'build_resources/libssl.a'),
+                          ('/usr/lib/libcrypto.a'               , 'build_resources/libcrypto.a'),
+                          ('/opt/freeware/lib/libffi.a'         , 'build_resources/libffi.a'),
                           ('/opt/freeware/lib/libgcc_s.a'       , 'build_resources/libgcc_s.a')]
 
     # Special includes for Solaris systems
