380ad47f17b67fe52797c84b41ce4421f3212d18
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-01-28 13:10:04 -0800

    run as nagios user without sudo

--- build/aix/aix-ncpa-service.sh
+++ build/aix/aix-ncpa-service.sh
@@ -37,7 +37,9 @@ start_ncpa() {
     cd "$NCPA_DIR"
     
     # Start NCPA as nagios user
-    sudo -u $NCPA_USER -g $NCPA_GROUP "$NCPA_DIR/ncpa" --start &
+    # sudo -u $NCPA_USER -g $NCPA_GROUP "$NCPA_DIR/ncpa" --start &
+    su $NCPA_USER -c "$NCPA_DIR/ncpa --start &"
+
     
     if [ $? -eq 0 ]; then
         echo "NCPA started successfully"
