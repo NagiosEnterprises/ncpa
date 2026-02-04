3e929c45b5bd5dd75e0885b4814ae289b2567f5c
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-02-04 10:29:57 -0800

    narrow down cause of error

--- build/build.sh
+++ build/build.sh
@@ -965,14 +965,14 @@ esac'
 
     # Set permissions (original working approach)
     echo -e "\nSetting permissions..."
-    sudo chmod -R g+r $BUILD_DIR/ncpa
-    sudo chmod -R a+r $BUILD_DIR/ncpa
-    sudo chown -R nagios:nagios $BUILD_DIR/ncpa/var
-    sudo chown nagios:nagios $BUILD_DIR/ncpa/etc $BUILD_DIR/ncpa/etc/*.cfg*
-    sudo chown nagios:nagios $BUILD_DIR/ncpa/etc/ncpa.cfg.d $BUILD_DIR/ncpa/etc/ncpa.cfg.d/*
-    sudo chmod 755 $BUILD_DIR/ncpa/etc $BUILD_DIR/ncpa/etc/ncpa.cfg.d
-    sudo chmod -R 755 $BUILD_DIR/ncpa/var
-    sudo chmod 755 $BUILD_DIR/ncpa
+    sudo chmod -v -R g+r $BUILD_DIR/ncpa
+    sudo chmod -v -R a+r $BUILD_DIR/ncpa
+    sudo chown -v -R nagios:nagios $BUILD_DIR/ncpa/var
+    sudo chown -v nagios:nagios $BUILD_DIR/ncpa/etc $BUILD_DIR/ncpa/etc/*.cfg*
+    sudo chown -v nagios:nagios $BUILD_DIR/ncpa/etc/ncpa.cfg.d $BUILD_DIR/ncpa/etc/ncpa.cfg.d/*
+    sudo chmod -v 755 $BUILD_DIR/ncpa/etc $BUILD_DIR/ncpa/etc/ncpa.cfg.d
+    sudo chmod -v -R 755 $BUILD_DIR/ncpa/var
+    sudo chmod -v 755 $BUILD_DIR/ncpa
 
     # Build tarball (original working approach)
     echo -e "\nBuilding tarball..."
