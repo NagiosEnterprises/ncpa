aaec611bdf4dba1e7be1f80ea1e55d186be75761
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-02-26 14:34:28 -0800

    redirect to global admin when authenticated

--- agent/listener/server.py
+++ agent/listener/server.py
@@ -471,7 +471,7 @@ def admin_login():
 
     if admin_password is not None and password_valid:
         session['admin_logged'] = True
-        return redirect(url_for('admin'))
+        return redirect(url_for('admin_global'))
     elif password is not None:
         template_args['error'] = 'Password was invalid.'
 
