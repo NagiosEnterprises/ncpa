c8f0322d53081d0a4357748817d0e69ad7b31271
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-03-02 08:48:12 -0800

    cleanup extra logging

--- agent/listener/server.py
+++ agent/listener/server.py
@@ -677,9 +677,6 @@ def help_section():
 def admin():
     tmp_args = {}
     tmp_args['config'] = listener.config['iconfig']
-
-    # Debug logging
-    listener_logger.info('Admin GUI accessed by %s', request.remote_addr)
     return render_template('admin/index.html', **tmp_args)
 
 
