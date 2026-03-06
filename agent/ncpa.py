013561912377cb8a6226698f3e7b516c231eaf41
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-03-06 08:07:31 -0800

    updating ssl_context dict

--- agent/ncpa.py
+++ agent/ncpa.py
@@ -328,7 +328,7 @@ class Listener(Base):
                                         log=listener_logger,
                                         error_log=listener_logger,
                                         spawn=Pool(max_connections),
-                                        **{'ssl_args': {'ssl_context': ssl_context}})
+                                        **{'ssl_context': ssl_context})
             logger.debug("run() - start http_server")
             http_server.serve_forever()
             logger.debug("run() - http_server running")
