22b3423fdf95ea0125589ea51c707d2da6632988
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-03-24 08:34:54 -0700

    test custom output

--- agent/listener/nodes.py
+++ agent/listener/nodes.py
@@ -517,17 +517,9 @@ class RunnableNode(ParentNode):
         listener_logger.debug("Values for %s: %r", proper_name, values)
 
         # Hack in the interface status change because we can't do much else...
-        # if self.name == "status":
-        #     # Replace 0 with up, 2 with down and 3 with unknown for the info line
-        #     if values[0] == 0:
-        #         readable_text = "up"
-        #     elif values[0] == 2:
-        #         readable_text = "down"
-        #     else values[0] == 3:
-        #         readable_text = "unknown"
-        #     # Also change the custom output to show the status instead of the numeric value
-        #     custom_output = proper_name + " is " + readable_text
-        #     values_for_info_line = ""
+        if self.name == "status":
+             custom_output = proper_name + " is " + values[0]
+             values_for_info_line = ""
 
         if secondary_data is True:
             stdout = "%s: %s" % (proper_name, values_for_info_line)
