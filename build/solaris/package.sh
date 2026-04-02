265511c0883f887a47d7b99a764f80981d3c4b09
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-04-02 12:52:07 -0700

    update path for config file

--- build/solaris/package.sh
+++ build/solaris/package.sh
@@ -74,7 +74,7 @@ rm -rf $DIR/pkginfo.tmp
     echo 'i preremove' >> prototype
 
     # Preserve ncpa.cfg file if it exists on the system during installation
-    echo 'f preserve /usr/local/ncpa/etc/ncpa.cfg=ncpa.cfg' >> prototype
+    echo 'f preserve /usr/local/ncpa/etc/ncpa.cfg=ncpa/etc/ncpa.cfg' >> prototype
     
     # Generate file list for the package
     echo "Generating file list..."
