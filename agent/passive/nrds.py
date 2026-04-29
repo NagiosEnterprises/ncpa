b5e2154c1268a555188ca7f4ee83ad576788f824
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-04-29 10:57:13 -0700

    try to read temp file

--- agent/passive/nrds.py
+++ agent/passive/nrds.py
@@ -103,8 +103,10 @@ class Handler(passive.nagioshandler.NagiosHandler):
                 temp_config.seek(0)
                 temp_config.write('[passive checks]\n')
 
+                logger.debug('reading file: %s', test_config.read_file(temp_config))
                 test_config = cp.ConfigParser()
                 test_config.read_file(temp_config) # problem here, missing section headers
+
                 logger.debug('Successfully read the temp config')
 
                 if not test_config.sections():
