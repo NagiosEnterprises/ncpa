fc26feca381091deb884fa7c28bb280247ff9f3b
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-06-15 10:06:12 -0700

    testing class update

--- agent/passive/nagioshandler.py
+++ agent/passive/nagioshandler.py
@@ -21,6 +21,7 @@ class NagiosHandler(object):
         """
         self.config = config
         self.checks = None
+        self.next_run = None
 
     def get_commands_from_config(self):
         """
