ad74babd40e8ddba098390ce80142618957c01ae
Coleman Dietsch <ne.cdietsch@gmail.com>
2026-07-30 16:46:45 -0500

    fix: remove near duplicate ssl_ciphers debug line

--- agent/ncpa.py
+++ agent/ncpa.py
@@ -283,7 +283,6 @@ class Listener(Base):
                 else:
                     logger.debug("run() - ssl_str_ciphers: %s", ssl_str_ciphers)
                     ssl_context.set_ciphers(ssl_str_ciphers)
-                logger.debug("ssl_str_ciphers: %s", ssl_str_ciphers)
 
                 # Get the SSL version from the config and set it on the SSL context
                 ssl_str_version = self.config.get('listener', 'ssl_version')
